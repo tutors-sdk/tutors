d79e117c98436392ab1d824659b24fc5ada547af
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-09-09 22:24:04 +0100

    feat: vitebook themes

--- design/tutors-components/src/[1]Themes/Themes.story.svelte
+++ design/tutors-components/src/[1]Themes/Themes.story.svelte
@@ -8,45 +8,13 @@
   
   <script>
     import { Variant } from '@vitebook/client';
-    import {
-      ControlsAddon,
-      EventsAddon,
-      eventCallback,
-    } from '@vitebook/client/addons';
-  
-    let title = 'Click Me';
-    let disabled = false;
     let themes = ["tutors", "tutors-dark", "tutors-black", "tutors-dyslexia", "tutors-wireframe"];
   </script>
   
-  <Variant name="Default" description="The default tutors theme.">
-    {#each themes as theme}
+  {#each themes as theme}
+  <Variant name={theme} description="The {theme} theme.">
     <div class="h-full bg-base-100" data-theme={theme}>
       The current selected theme is {theme}
   </div>
+  </Variant>
   {/each}
-  </Variant>
-  
-  <Variant
-    name="Disabled"
-    description="The disabled button."
-    on:enter={() => {
-      disabled = true;
-    }}
-    on:exit={() => {
-      disabled = false;
-    }}
-  >
-  </Variant>
-  
-  <ControlsAddon>
-    <div>
-      Title <input type="text" bind:value={title} />
-    </div>
-    <div class="mt-6">
-      Disabled <input type="checkbox" bind:checked={disabled} />
-    </div>
-  </ControlsAddon>
-  
-  <EventsAddon />
-  
