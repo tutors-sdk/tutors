da31f2fe02a63870f5285ce42547b5796cb25230
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-10-20 12:19:19 +0100

    fix: student card

--- sites/tutors-course-reader/src/components/cards/StudentCard.svelte
+++ sites/tutors-course-reader/src/components/cards/StudentCard.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import type { StudentMetric } from "tutors-reader-lib/src/types/metrics-types";
-  import { getIcon } from "tutors-reader-lib/src/iconography/themes";
   import { cardTransition } from "../animations";
   import Iconify from "@iconify/svelte";
+  import { Avatar } from "@brainandbones/skeleton";
 
   export let student: StudentMetric;
 
   let headingText = "text-xs font-medium";
-  let cardWidths = "w-36 h-56";
-  let cardType = "tutorscard-compact";
   let iconHeight = "80";
   let imageHeight = "h-24";
 
@@ -26,18 +24,16 @@
   }
 </script>
 
-<div transition:cardTransition class="{cardType} {cardWidths} border-{getIcon('talk').colour}">
+<div transition:cardTransition class="card w-36 h-56 overflow-x-hidden m-1">
   <div class="flex">
-    <div class="avatar">
-      <div class="mt-1 ml-1 w-10 rounded-full">
-        <img loading="lazy" class="object-scale-down" src={student.img} alt={student.nickname} />
-      </div>
-    </div>
-    <div class="text-center {headingText} mt-3 mr-1">{student.name}</div>
+    <header class="card-header inline-flex">
+      <Avatar src={student.img} alt={student.nickname} />
+    <h6>{student.name}</h6>
+  </header>
   </div>
-
-  {#if student.topic && !student.lab}
-    <div class="mx-auto my-2">
+    <div class="card-body">
+      {#if student.topic && !student.lab}
+    <div class="justify-center my-2">
       {#if student.topic.lo.icon}
         <Iconify icon={student.topic.lo.icon.type} color={student.topic.lo.icon.color} height={iconHeight} />
       {:else}
@@ -46,7 +42,7 @@
     </div>
   {/if}
   {#if student.lab && !student.topic}
-    <div class="mx-auto my-2">
+    <div class="justify-center my-2">
       {#if student.lab.icon}
         <Iconify icon={student.lab.icon.type} color={student.lab.icon.color} height={iconHeight} />
       {:else}
@@ -55,12 +51,7 @@
     </div>
   {/if}
   {#if student.topic && student.lab}
-    <div class="mx-auto my-1 flex">
-      {#if student.topic.lo.icon}
-        <Iconify icon={student.topic.lo.icon.type} color={student.topic.lo.icon.color} height={iconHeight} />
-      {:else}
-        <img loading="lazy" class="tutorscard-image {imageHeight}" src={student?.topic?.lo.img} alt={student?.topic?.lo.title} />
-      {/if}
+    <div class="justify-center my-1 flex">
       {#if student.lab.icon}
         <Iconify icon={student.lab.icon.type} color={student.lab.icon.color} height={iconHeight} />
       {:else}
@@ -68,20 +59,21 @@
       {/if}
     </div>
   {/if}
-  <div class="tutorscard-body text-center text-xs">
-    <div class="mt-2 italic">
-      {new Date(student.time).toLocaleTimeString()}
     </div>
-    <div class="mt-2">
-      {#if student.topic}
-        <div><span class="italic" />{student.topic.lo.title}</div>
-      {/if}
-      {#if student.lab}
-        <div><span class="italic" />{student.lab.title}</div>
-      {/if}
-    </div>
+    <footer class="card-footer text-center">
+      <p class="mt-2 italic">
+        {new Date(student.time).toLocaleTimeString()}
+      </p>
+      <p class="mt-2 font-semibold">
+        {#if student.topic}
+          <div><span class="italic" />{student.topic.lo.title}</div>
+        {/if}
+        {#if student.lab}
+          <div><span class="italic" />{student.lab.title}</div>
+        {/if}
+      </p>
+    </footer>
   </div>
-</div>
 
 <style>
   .card {
