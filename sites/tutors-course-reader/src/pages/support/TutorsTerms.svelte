016ca3091b6f12e9db4e8324be5ae58a1a4711d9
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-10-28 16:10:31 +0100

    feat: tutors terms design + mobile fix

--- sites/tutors-course-reader/src/pages/support/TutorsTerms.svelte
+++ sites/tutors-course-reader/src/pages/support/TutorsTerms.svelte
@@ -6,12 +6,11 @@
   }
 </script>
 
-<div class="bg-neutral text-neutral-content flex h-screen items-center justify-center bg-opacity-90 backdrop-blur">
-  <div class="max-w-screen-md text-center">
+<div class="bg-surface-600 min-h-screen flex items-center justify-center overflow-auto">
     <div
-      class="bg-base-200 text-base-content m-8 flex flex-wrap justify-items-center rounded-lg bg-opacity-90 px-4 py-2 shadow-md backdrop-blur"
+      class="card m-8 flex flex-wrap justify-items-center p-4 shadow-xl"
     >
-      <div class="flex w-full justify-between border-b-2 border-gray-400 p-2">
+      <div class="flex w-full justify-between border-b-2 border-surface-300 p-2">
         <div class="font-sm text-center text-xl font-light">TutorsTime</div>
       </div>
 
@@ -19,13 +18,9 @@
         <p class="p-2">
           You have been authenticated via your GitHub credentials and are about to enter a Tutors course. Tutors will record how much time
           each view is active and send this to the TutorsTime data store. You can view this information via the TutorsTime feature on the
-          course Navigator.
-        </p>
-        <p class="p-2">
+          course Navigator.<br /><br />
           This data is available to you + the instructor of your course, but not to other students. No other data is gathered, nor is this
-          data transmitted anywhere other than the TutorsTime data store.
-        </p>
-        <p class="p-2">
+          data transmitted anywhere other than the TutorsTime data store.<br /><br />
           TutorsLive also uses this data to display of panel of students currently online. You can opt out of this feature from the
           TutorsLive page.
         </p>
@@ -45,7 +40,6 @@
         </p>
         <p class="p-2">You will be asked to authenticate again in seven days time.</p>
       </div>
-      <button class="btn btn-rounded btn-info" on:click={acceptAuthenticate}>Proceed to Course </button>
-    </div>
+      <button class="btn bg-primary-500 text-white" on:click={acceptAuthenticate}>Proceed to Course </button>
   </div>
 </div>
