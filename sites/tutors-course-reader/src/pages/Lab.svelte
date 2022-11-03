618643dd7e5536825bb458da1755258f741a4676
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-03 09:32:55 +0000

    fix: prose size

--- sites/tutors-course-reader/src/pages/Lab.svelte
+++ sites/tutors-course-reader/src/pages/Lab.svelte
@@ -110,7 +110,7 @@
           </nav>
         </header>
         <div class="card bg-surface-100-800-token p-8 lg:px-4 py-8 m-2 rounded-xl">
-          <article class="mx-auto prose dark:prose-invert lg:prose-lg prose-img:max-w-[110%] prose-img:ml-[-5%]">
+          <article class="mx-auto prose dark:prose-invert lg:prose-lg max-w-full lg:max-w-[85%]">
             {@html lab.content}
           </article>
         </div>
