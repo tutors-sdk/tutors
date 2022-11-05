fdc66c1e38663fe5c84add5d1289918c29fec53f
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-05 13:08:04 +0000

    feat: migrate tutors-home to skeleton

--- sites/tutors-home/astro.config.mjs
+++ sites/tutors-home/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
