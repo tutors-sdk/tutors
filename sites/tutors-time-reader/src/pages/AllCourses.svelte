f4d799050d36d302afa5ee695c8c053aa99b33cb
Eamonn de Leastar <edeleastar@wit.ie>
2022-11-22 06:38:35 +0000

    refactor: simplify time views

--- sites/tutors-time-reader/src/pages/AllCourses.svelte
+++ sites/tutors-time-reader/src/pages/AllCourses.svelte
@@ -1,53 +1,45 @@
 <script lang="ts">
-  import { Card } from "tutors-ui";
-  import type { Lo } from "tutors-reader-lib/src/types/lo-types";
-  import { fetchAllCourseAccess } from "tutors-reader-lib/src/utils/firebase-utils";
-  import { getCourseSummary, isValidCourseName } from "tutors-reader-lib/src/utils/course-utils";
-  import { toHoursAndMinutes } from "tutors-reader-lib/src/utils/metrics-utils";
-  import { portfolio } from "tutors-reader-lib/src/stores/stores";
+  import { CourseCardDeck } from "tutors-ui";
+  import { fetchAllCourseAccess, deleteObj } from "tutors-reader-lib/src/utils/firebase-utils";
+  import { getCourseSummary, isValidCourseName, type CourseSummary } from "tutors-reader-lib/src/utils/course-utils";
   import NavBar from "../navigators/NavBar.svelte";
 
-  let los: Lo[] = [];
+  let los: CourseSummary[] = [];
   $: tickerTape = "";
   $: modules = 0;
   let visits = 0;
 
-  function summarise(usage: any): string {
-    let str = "Time ";
-    str += `${toHoursAndMinutes(usage.count)}<br>`;
-    str += "Page Loads: ";
-    str += `${usage.visits}`;
-    visits += usage.visits;
-    return str;
-  }
-
   void getAllCourses();
 
   async function populate(allCourseAccess: any[]) {
-    const localLos: Lo[] = [];
-    // total = allCourseAccess.length;
+    let courseId = "";
     for (let i = 0; i < allCourseAccess.length; i++) {
       try {
-        const courseId = allCourseAccess[i].courseId;
-        const lo = await getCourseSummary(courseId);
-        const keepPrivate = lo.properties?.private as unknown as number;
-        if (!keepPrivate) {
+        courseId = allCourseAccess[i].courseId;
+        const courseSummary = await getCourseSummary(courseId);
+        courseSummary.visits = allCourseAccess[i].visits;
+        courseSummary.count = allCourseAccess[i].count;
+        visits += courseSummary.visits;
+        if (!courseSummary.isPrivate) {
           modules++;
-          tickerTape = `${lo.title}`;
-          lo.summary = summarise(allCourseAccess[i]);
-          localLos.push(lo);
-          los = [...localLos];
-          los.sort((lo1: Lo, lo2: Lo) => lo1.title.localeCompare(lo2.title));
+          tickerTape = `${courseSummary.title}`;
+          los.push(courseSummary);
+          los = [...los];
+          los.sort((lo1: CourseSummary, lo2: CourseSummary) => lo1.title.localeCompare(lo2.title));
         }
       } catch (error: any) {
-        console.log(`invalid course :${error.message}`);
+        deleteObj("all-course-access", courseId);
+        console.log(`invalid course ${courseId} : ${error.message}`);
       }
     }
   }
 
   async function getAllCourses() {
-    portfolio.set(true);
     let allCourseAccess = await fetchAllCourseAccess();
+    const invalidCourses = allCourseAccess.filter((usage) => !isValidCourseName(usage.courseId));
+    invalidCourses.forEach((course) => {
+      deleteObj("all-course-access", course.courseId);
+    });
     allCourseAccess = allCourseAccess.filter((usage) => isValidCourseName(usage.courseId));
     allCourseAccess = allCourseAccess.filter((usage) => usage?.visits > 50);
     await populate(allCourseAccess);
@@ -60,14 +52,4 @@
 <div class="sticky top-0 z-40 mb-5">
   <NavBar title="Tutors Module Ecosystem" subTitle={tickerTape} {modules} {visits} />
 </div>
-{#if los.length}
-  <div
-    class="bg-surface-100-800-token rounded-box card-corner mx-auto mb-2 w-11/12 max-w-full place-items-center overflow-hidden rounded-xl p-4 backdrop-blur"
-  >
-    <div class="flex flex-wrap justify-center">
-      {#each los as lo}
-        <Card {lo} />
-      {/each}
-    </div>
-  </div>
-{/if}
+<CourseCardDeck {los} />
--- sites/tutors-time-reader/src/pages/CourseEvents.svelte
+++ sites/tutors-time-reader/src/pages/CourseEvents.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import { Card } from "tutors-ui";
-  import type { Lo } from "tutors-reader-lib/src/types/lo-types";
+  import { CourseCardDeck } from "tutors-ui";
+  import { getCourseSummary, type CourseSummary } from "tutors-reader-lib/src/utils/course-utils";
   import { fetchAllCourseAccess } from "tutors-reader-lib/src/utils/firebase-utils";
   import { child, get, getDatabase, onValue, ref } from "firebase/database";
-  import { toHoursAndMinutes } from "tutors-reader-lib/src/utils/metrics-utils";
   import NavBar from "../navigators/NavBar.svelte";
 
-  let los: Lo[] = [];
-  let courseMap = new Map<string, any>();
+  let los: CourseSummary[] = [];
+  let courses = new Map<string, CourseSummary>();
   let modules = 0;
   let title = "Tutors Module Activity";
   $: subTitle = "Connecting ...";
@@ -34,59 +33,37 @@
 
   void startListening();
 
-  function summarise(usage: any, visits: number): string {
-    let str = "";
-    if (usage.count) str += `Time ${toHoursAndMinutes(usage.count)}: `;
-    str += `Visits: ${usage.visits}: `;
-    str += `Count: ${visits}: `;
-    str += `<br>${usage.title}`;
-    return str;
-  }
-
-  function updateNmrUsers(lo: any) {
-    if (lo.tutorsTimeId) {
-      tutorsTimeIds.add(lo.tutorsTimeId);
+  function updateNmrUsers(courseSummary: CourseSummary) {
+    if (courseSummary.currentLo.tutorsTimeId) {
+      tutorsTimeIds.add(courseSummary.currentLo.tutorsTimeId);
       users = tutorsTimeIds.size;
     }
   }
 
-  function usageUpdate(courseId: string, usage: any) {
-    const lo = usage.lo;
-    const foundLo = courseMap.get(courseId);
-    if (foundLo) {
-      foundLo.title = lo.courseTitle;
-      foundLo.visits++;
-      foundLo.summary = summarise(usage, foundLo.visits);
-      foundLo.tutorsTimeId = lo.tutorsTimeId;
-      updateNmrUsers(foundLo);
-      foundLo.route = `https://reader.tutors.dev${lo.subRoute}`;
-      if (lo.img) {
-        foundLo.img = lo.img;
-        if (lo.icon) {
-          foundLo.icon = lo.icon;
-        } else {
-          foundLo.icon = null;
-        }
-      }
+  function updateCourseSummary(courseSummary: CourseSummary) {
+    courseSummary.route = `https://reader.tutors.dev${courseSummary.currentLo?.subRoute}`;
+    courseSummary.img = courseSummary.currentLo?.img;
+    if (courseSummary.currentLo.icon) {
+      courseSummary.icon = courseSummary.currentLo.icon;
     } else {
-      if (!lo.isPrivate) {
-        const loCopy = { ...lo };
-        loCopy.title = lo.courseTitle;
-        loCopy.route = `https://reader.tutors.dev${lo.subRoute}`;
-        loCopy.visits = 1;
-        loCopy.summary = summarise(usage, 1);
-        loCopy.type = "web";
-        courseMap.set(courseId, loCopy);
-        los.push(loCopy);
-        updateNmrUsers(lo);
-      }
+      courseSummary.icon = null;
     }
   }
-
   async function visitUpdate(courseId: string) {
+    let courseSummary = courses.get(courseId);
+    if (!courseSummary) {
+      courseSummary = await getCourseSummary(courseId);
+      if (courseSummary.isPrivate) return;
+      courses.set(courseId, courseSummary);
+      los.push(courseSummary);
+    }
     const usage = await (await get(child(ref(db), `all-course-access/${courseId}`))).val();
     if (usage.lo) {
-      usageUpdate(courseId, usage);
+      courseSummary.currentLo = usage.lo;
+      courseSummary.count = usage.count;
+      courseSummary.visits = usage.visits;
+      updateCourseSummary(courseSummary);
+      updateNmrUsers(courseSummary);
       los = [...los];
       modules = los.length;
       subTitle = `Active since : ${activeSince}`;
@@ -114,14 +91,4 @@
 <div class="sticky top-0 z-40 mb-5">
   <NavBar {title} {subTitle} {modules} {visits} {users} />
 </div>
-{#if los.length}
-  <div
-    class="bg-surface-100-800-token rounded-box card-corner mx-auto mb-2 w-11/12 max-w-full place-items-center overflow-hidden rounded-xl p-4 backdrop-blur"
-  >
-    <div class="flex flex-wrap justify-center">
-      {#each los as lo}
-        <Card {lo} />
-      {/each}
-    </div>
-  </div>
-{/if}
+<CourseCardDeck {los} />
