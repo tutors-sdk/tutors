7b3a4a8b2c5046d4d67fd950d9989cc08fc4a4ad
Eamonn de Leastar <edeleastar@wit.ie>
2022-12-09 16:18:22 +0000

    fix: corrected first step double encoding

--- apps/course/src/routes/lab/[courseid]/[...loid]/+page.ts
+++ apps/course/src/routes/lab/[courseid]/[...loid]/+page.ts
@@ -5,8 +5,7 @@ export const ssr = false;
 
 export const load: PageLoad = async ({ url, params }) => {
   const lab = await courseService.readLab(params.courseid, url.pathname);
-  const encoded = encodeURI(url.pathname);
-  const lastSegment = encoded.substr(url.pathname.lastIndexOf("/") + 1);
+  const lastSegment = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
   if (lastSegment.startsWith("book")) {
     lab.setFirstPageActive();
   } else {
