e3df68b318b08256d2ea356815b137ff037f5c53
Eamonn de Leastar <edeleastar@wit.ie>
2022-12-13 11:40:37 +0000

    feat: additional card for revised presence service

--- packages/tutors-ui/index.ts
+++ packages/tutors-ui/index.ts
@@ -13,11 +13,13 @@ export { default as NavUser } from "./lib/Molecules/NavUser/NavUser.svelte";
 export { default as NavTimeUser } from "./lib/Molecules/NavUser/NavTimeUser.svelte";
 export { default as NoteCard } from "./lib/Molecules/Cards/NoteCard.svelte";
 export { default as StudentCard } from "./lib/Molecules/Cards/StudentCard.svelte";
+export { default as PrimaryStudentCard } from "./lib/Molecules/Cards/PrimaryStudentCard.svelte";
 export { default as TalkCard } from "./lib/Molecules/Cards/TalkCard.svelte";
 export { default as TopicNavigatorCard } from "./lib/Molecules/Cards/TopicNavigatorCard.svelte";
 export { default as VideoCard } from "./lib/Molecules/Cards/VideoCard.svelte";
 
 export { default as CardDeck } from "./lib/Organisms/CardDeck/CardDeck.svelte";
 export { default as CourseCardDeck } from "./lib/Organisms/CardDeck/CourseCardDeck.svelte";
+export { default as StudentCardDeck } from "./lib/Organisms/CardDeck/StudentCardDeck.svelte";
 export { default as TopicNavigator } from "./lib/Organisms/TopicNavigator/TopicNavigator.svelte";
 export { default as UnitCard } from "./lib/Organisms/UnitCard/UnitCard.svelte";
---
+++ packages/tutors-ui/lib/Organisms/CardDeck/StudentCardDeck.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { PrimaryStudentCard } from "tutors-ui";
+  import { StudentLoEvent } from "tutors-reader-lib/src/types/metrics-types";
+  export let los: StudentLoEvent[] = [];
+</script>
+
+{#if los.length}
+  <div class="bg-surface-100-800-token rounded-box card-corner mx-auto mb-2 w-11/12 max-w-full place-items-center overflow-hidden rounded-xl p-4 backdrop-blur">
+    <div class="flex flex-wrap justify-center">
+      {#each los as lo}
+        <PrimaryStudentCard lo="{lo}" />
+      {/each}
+    </div>
+  </div>
+{/if}
