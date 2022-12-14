0e41277e3a6f744a5f6039c7d8947ee8d4dbbf34
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-12-14 12:43:49 +0000

    fix: minor design updates

--- apps/course/src/lib/navigators/sidebars/OnlineBar.svelte
+++ apps/course/src/lib/navigators/sidebars/OnlineBar.svelte
@@ -10,7 +10,7 @@
 <div class="mt-4 mr-4 text-right">
   <button class="btn btn-icon bg-primary-500 text-white" on:click="{drawerClose}"><span class="font-bold">X</span></button>
 </div>
-<div class="px-12 py-4">
+<div class="px-6 py-4">
   <div class="flex flex-wrap">
     {#if $studentsOnline == 0}
       <span class="text-lg">No students currently online...</span>
--- packages/tutors-ui/lib/Organisms/CardDeck/StudentCardDeck.svelte
+++ packages/tutors-ui/lib/Organisms/CardDeck/StudentCardDeck.svelte
@@ -3,10 +3,8 @@
   import { studentsOnlineList } from "tutors-reader-lib/src/stores/stores";
 </script>
 
-<div class="bg-surface-100-800-token rounded-box card-corner mx-auto mb-2 w-11/12 max-w-full place-items-center overflow-hidden rounded-xl p-4 backdrop-blur">
   <div class="flex flex-wrap justify-center">
     {#each $studentsOnlineList as studentLo}
       <PrimaryStudentCard lo="{studentLo}" />
     {/each}
   </div>
-</div>
