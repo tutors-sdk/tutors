34147c6bf61ea9f6fd50807f798ffc8733879530
Eamonn de Leastar <edeleastar@wit.ie>
2022-12-19 09:33:51 +0000

    fix: access to localStorageStore fails on production build on netlify

--- packages/tutors-reader-lib/src/stores/stores.ts
+++ packages/tutors-reader-lib/src/stores/stores.ts
@@ -22,7 +22,7 @@ export const currentUser: Writable<User> = writable();
 export const portfolio = writable(false);
 export const layout = writable("");
 export const transitionKey = writable("");
-export const storeTheme: Writable<string> = localStorageStore("storeTheme", "tutors");
+export const storeTheme: Writable<string> = writable("tutors"); //localStorageStore("storeTheme", "tutors");
 export const authenticating: Writable<boolean> = writable(false);
 
 const students: StudentLoEvent[] = [];
