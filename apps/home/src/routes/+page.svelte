ca2254e58bd7cd7dd944b3a6762634dd84abaf86
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-12-20 10:57:06 +0000

    feat: open source section

--- apps/home/src/routes/+page.svelte
+++ apps/home/src/routes/+page.svelte
@@ -60,3 +60,17 @@
 		</div></a>
 	</div>
 </div>
+<div class="bg-gradient-to-l from-primary-50 dark:from-primary-900 to-accent-50 dark:to-accent-900">
+	<div class="container lg:flex justify-center items-center mx-auto py-20">
+	<div class="w-full mx-4">
+		<h1 class="font-bold text-6xl inline-block my-4">A fully <GradientHeading class="font-bold !text-6xl inline-block">Open Source</GradientHeading> project</h1>
+		<p class="font-bold !text-xl my-4">Tutors is an open source project available for free under the MIT license on GitHub. It exists due to the hard work and dedication of all individuals who have contributed to project.</p>
+		<a href="https://github.com/tutors-sdk/tutors/graphs/contributors" target="_blank" rel="noreferrer">
+			<img src="https://contrib.rocks/image?repo=tutors-sdk/tutors" alt="github contributors list" />
+		  </a>
+		<div class="my-4">
+			<a class="btn btn-xl bg-primary-500 text-white font-bold hover:scale-105 transition-all" href="https://github.com/tutors-sdk/tutors" target="_blank" rel="noreferrer"><iconify-icon icon="mdi:github" />&nbsp; View Source Code</a>
+		</div>
+	</div>
+	</div>
+</div>
