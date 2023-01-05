0932b26e9ed487753ad93027c1687748ffc293da
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-05 13:58:20 +0000

    feature: builder version of generator in separate app (for the moment)

---
+++ cli/tutors-gen/src/builders/lo-types.ts
@@ -0,0 +1,57 @@
+export class Properties {
+  [key: string]: string;
+}
+
+export interface VideoIdentifier {
+  service: string;
+  id: string;
+}
+
+export interface VideoIdentifiers {
+  videoid: string;
+  videoIds: VideoIdentifier[];
+}
+
+export interface LearningResource {
+  courseRoot: string;
+  route: string;
+  id: string;
+  lrs: LearningResource[];
+  files: Array<string>;
+  type: string;
+}
+
+export interface LearningObject {
+  id: string;
+  route: string;
+  type: string;
+  title: string;
+  summary: string;
+  img: string;
+  pdf: string;
+  contentMd: string;
+  video: string;
+  videoids: VideoIdentifiers;
+  frontMatter: Properties;
+  los: LearningObject[];
+  properties?: Properties;
+  calendar?: Properties;
+  hide: boolean;
+}
+
+export const loTypes = ["/note", "/book", "/archive", "/web", "/github", "/panelnote", "/paneltalk", "/panelvideo", "/talk", "/unit", "/topic"];
+
+export const preOrder = new Map([
+  ["unit", 1],
+  ["talk", 2],
+  ["lab", 3],
+  ["note", 4],
+  ["web", 5],
+  ["github", 6],
+  ["panelnote", 7],
+  ["paneltalk", 8],
+  ["archive", 9],
+  ["panelvideo", 10],
+  ["topic", 11],
+  ["unknown", 12],
+]);
