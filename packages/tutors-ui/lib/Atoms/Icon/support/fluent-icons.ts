1c75e057a2e1bc51d816d1ee34e482c9fc2a38eb
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-01-06 12:43:03 +0000

    feat: color name change to new convention

--- packages/tutors-ui/lib/Atoms/Icon/support/fluent-icons.ts
+++ packages/tutors-ui/lib/Atoms/Icon/support/fluent-icons.ts
@@ -7,21 +7,21 @@ export const FluentIconLib = {
   moodle: { icon: "academicons:moodle", colour: "warning" },
   youtube: { icon: "fa:youtube", colour: "error" },
   video: { icon: "fluent:video-clip-24-filled", colour: "error" },
-  zoom: { icon: "fluent:video-24-filled", colour: "info" },
+  zoom: { icon: "fluent:video-24-filled", colour: "primary" },
   teams: { icon: "logos:microsoft-teams", colour: "bg-base-content" },
 
   // LoTypes icons
-  course: { icon: "fluent:notebook-24-filled", colour: "info" },
-  topic: { icon: "fluent:bookmark-24-filled", colour: "info" },
+  course: { icon: "fluent:notebook-24-filled", colour: "primary" },
+  topic: { icon: "fluent:bookmark-24-filled", colour: "primary" },
   unit: { icon: "fluent:dual-screen-group-24-filled", colour: "success" },
-  talk: { icon: "fluent:presenter-24-filled", colour: "info" },
+  talk: { icon: "fluent:presenter-24-filled", colour: "primary" },
   reference: { icon: "fluent:document-copy-24-filled", colour: "warning" },
   lab: { icon: "fluent:beaker-24-filled", colour: "success" },
-  archive: { icon: "fluent:archive-24-filled", colour: "info" },
+  archive: { icon: "fluent:archive-24-filled", colour: "primary" },
   web: { icon: "fluent:bookmark-24-regular", colour: "error" },
   github: { icon: "fluent:code-circle-20-filled", colour: "warning" },
   panelvideo: { icon: "fluent:video-clip-24-regular", colour: "error" },
-  paneltalk: { icon: "fluent:presenter-24-filled", colour: "info" },
+  paneltalk: { icon: "fluent:presenter-24-filled", colour: "primary" },
   note: { icon: "fluent:notepad-16-regular", colour: "warning" },
   panelnote: { icon: "fluent:notepad-16-regular", colour: "warning" },
 
@@ -36,22 +36,22 @@ export const FluentIconLib = {
   // tutors time icons
   online: { icon: "fluent:presence-available-24-filled", colour: "success" },
   offline: { icon: "fluent:presence-available-24-regular", colour: "error" },
-  tutorsTime: { icon: "fluent:clock-alarm-24-filled", colour: "info" },
+  tutorsTime: { icon: "fluent:clock-alarm-24-filled", colour: "primary" },
   timeExport: { icon: "fluent:save-arrow-right-24-filled", colour: "success" },
   live: { icon: "fluent:people-community-24-filled", colour: "success" },
 
   // app icons
-  search: { icon: "fluent:search-24-filled", colour: "info" },
+  search: { icon: "fluent:search-24-filled", colour: "primary" },
   tutors: { icon: "fa-solid:chalkboard-teacher", colour: "bg-base-content" },
   logout: { icon: "fluent:sign-out-24-filled", colour: "error" },
   dark: { icon: "fluent:paint-brush-24-filled", colour: "warning" },
   toc: { icon: "fluent:line-horizontal-3-20-filled", colour: "bg-base-content" },
   compacted: { icon: "fluent:re-order-dots-vertical-24-filled", colour: "success" },
   expanded: { icon: "fluent:re-order-dots-horizontal-24-filled", colour: "success" },
-  courseinfo: { icon: "fluent:info-28-regular", colour: "bg-base-content" },
+  courseprimary: { icon: "fluent:primary-28-regular", colour: "bg-base-content" },
   calendar: { icon: "fluent:calendar-ltr-12-regular", colour: "bg-base-content" },
-  appSettings: { icon: "fluent:settings-24-filled", colour: "info" },
-  listOnline: { icon: "fluent:people-list-24-filled", colour: "info" },
+  appSettings: { icon: "fluent:settings-24-filled", colour: "primary" },
+  listOnline: { icon: "fluent:people-list-24-filled", colour: "primary" },
 
   default: { icon: "fluent:re-order-dots-vertical-24-filled", colour: "error" }
 };
