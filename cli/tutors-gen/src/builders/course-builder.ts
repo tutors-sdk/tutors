9f6858bb587c40700a87dfa1e8264a9a12979cc9
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-08 12:48:18 +0000

    refactor: simplify builder

--- cli/tutors-gen/src/builders/course-builder.ts
+++ cli/tutors-gen/src/builders/course-builder.ts
@@ -1,12 +1,31 @@
 import { getFilesWithType, getFileWithName, getId, getImage, getLabImage, getMarkdown, getPdf, getRoute, getVideo, readVideoIds } from "../utils/lr-utils";
 import { LearningObject, LearningResource, preOrder } from "./lo-types";
-import { copyFileToFolder, readWholeFile, readYamlFile, writeFile } from "../utils/utils";
+import { readWholeFile, readYamlFile, writeFile } from "../utils/utils";
 import fm from "front-matter";
 
 export const courseBuilder = {
   lo: <LearningObject>{},
 
   buildLo(lr: LearningResource, level: number): LearningObject {
+    let lo = this.buildDefaultLo(lr);
+    console.log(`${"-".repeat(level * 2)}: ${lo.id} : ${lo.title}`);
+    if (lo.type === "lab") {
+      lo = this.buildLab(lo, lr);
+    } else {
+      if (lo.type === "unit") {
+        lo.route = lo.route.substring(0, lo.route.lastIndexOf("/")) + "/";
+        lo.route = lo.route.replace("/unit", "/topic");
+      }
+      lr.lrs.forEach((lr) => {
+        lo.los.push(this.buildLo(lr, level + 1));
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        lo.los.sort((a, b) => preOrder.get(a.type)! - preOrder.get(b.type)!);
+      });
+    }
+    return lo;
+  },
+
+  buildDefaultLo(lr: LearningResource): LearningObject {
     const [title, summary, contentMd, frontMatter] = getMarkdown(lr);
     const videoids = readVideoIds(lr);
     const lo: LearningObject = {
@@ -24,27 +43,10 @@ export const courseBuilder = {
       los: [],
       hide: false,
     };
-    if (lo.type === "lab") {
-      lo.los = this.buildLab(lr);
-      lo.img = getLabImage(lr);
-      if (lo.los.length > 0) {
-        lo.title = lo.los[0].shortTitle;
-      }
-    } else if (lo.type === "unit") {
-      lo.route = lo.route.substring(0, lo.route.lastIndexOf("/")) + "/";
-      lo.route = lo.route.replace("/unit", "/topic");
-    } else
-      lr.lrs.forEach((lr) => {
-        lo.los.push(this.buildLo(lr, level + 1));
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        lo.los.sort((a, b) => preOrder.get(a.type)! - preOrder.get(b.type)!);
-      });
-    console.log(`${"-".repeat(level * 2)}: ${lo.id} : ${lo.title}`);
     return lo;
   },
 
-  buildLab(lr: LearningResource): LearningObject[] {
-    const los: any = [];
+  buildLab(lo: LearningObject, lr: LearningResource): LearningObject {
     const mdFiles = getFilesWithType(lr, "md");
     mdFiles.forEach((chapterName) => {
       const wholeFile = readWholeFile(chapterName);
@@ -52,16 +54,20 @@ export const courseBuilder = {
       let theTitle = contents.body.substr(0, contents.body.indexOf("\n"));
       theTitle = theTitle.replace("\r", "");
       const shortTitle = chapterName.substring(chapterName.indexOf(".") + 1, chapterName.lastIndexOf("."));
-      const lo = {
+      const labStep = {
         title: theTitle,
         shortTitle: shortTitle,
         contentMd: contents.body,
         route: `${getRoute(lr)}/${shortTitle}`,
         type: "labstep",
       };
-      los.push(lo);
+      lo.los.push(labStep);
     });
-    return los;
+    lo.img = getLabImage(lr);
+    if (lo.los.length > 0) {
+      lo.title = lo.los[0].shortTitle;
+    }
+    return lo;
   },
 
   buildCourse(lr: LearningResource) {
@@ -86,7 +92,6 @@ export const courseBuilder = {
   },
 
   generateCourse(outputFolder: string) {
-    copyFileToFolder("course.png", outputFolder);
     writeFile(process.cwd(), `${outputFolder}/tutors.json`, JSON.stringify(this.lo));
   },
 };
