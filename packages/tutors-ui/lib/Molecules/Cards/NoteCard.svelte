c8ee2c4ae876e402a4d5b7989dd778f5ef8a1e85
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-09 11:09:49 +0000

    fix: correct url for images in note card

--- packages/tutors-ui/lib/Molecules/Cards/NoteCard.svelte
+++ packages/tutors-ui/lib/Molecules/Cards/NoteCard.svelte
@@ -2,10 +2,14 @@
   import type { Lo } from "tutors-reader-lib/src/types/lo-types";
   import { convertMd } from "tutors-reader-lib/src/utils/markdown-utils";
   import { generateToc } from "tutors-reader-lib/src/utils/markdown-toc-lib";
+  import { currentCourse } from "tutors-reader-lib/src/stores/stores";
 
   export let lo: Lo;
   let contentHtml = "";
-  const url = lo.route.replace("/#/note/", "");
+  let url = lo.route.replace("/note/", "");
+  const courseId = $currentCourse.id;
+  const courseUrl = $currentCourse.url;
+  url = url.replace(courseId, courseUrl);
   contentHtml = convertMd(lo.contentMd, url.replace("/note/", ""));
   contentHtml = generateToc(contentHtml);
 </script>
