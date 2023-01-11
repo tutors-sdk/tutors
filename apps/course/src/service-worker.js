c312f765033801f133bad32e316c5f5ebaf4294d
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-01-11 16:19:53 +0000

    Revert "test"
    This reverts commit cae0edec90a7ca56ec974c321eec0aa7ea20174a.

--- apps/course/src/service-worker.js
+++ apps/course/src/service-worker.js
@@ -18,7 +18,13 @@ const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ["./", "/_app/immutable/assets"];
+const PRECACHE_URLS = [
+  "index.html",
+  "./", // Alias for index.html
+  "styles.css",
+  "../../styles/main.css",
+  "demo.js"
+];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
