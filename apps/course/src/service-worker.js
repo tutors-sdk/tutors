cae0edec90a7ca56ec974c321eec0aa7ea20174a
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-01-11 16:13:05 +0000

    test

--- apps/course/src/service-worker.js
+++ apps/course/src/service-worker.js
@@ -18,13 +18,7 @@ const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  "index.html",
-  "./", // Alias for index.html
-  "styles.css",
-  "../../styles/main.css",
-  "demo.js"
-];
+const PRECACHE_URLS = ["./", "/_app/immutable/assets"];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
