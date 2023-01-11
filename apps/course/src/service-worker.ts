da123f9eb4721f17c0ca4f579a6ce952c9c63e5b
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-01-11 15:54:56 +0000

    Update service-worker.ts

--- apps/course/src/service-worker.ts
+++ apps/course/src/service-worker.ts
@@ -1,12 +1,61 @@
-import { build, files, prerendered, version } from "$service-worker";
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-const precache_list = [
-  "/", // Attention: might not be ideal for your use case - read more below.
-  ...build,
-  ...files,
-  ...prerendered
-].map((s) => ({
-  url: s,
-  revision: version
-}));
-precacheAndRoute(precache_list);
+import { build, files, version } from "$service-worker";
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+  ...build, // the app itself
+  ...files // everything in `static`
+];
+
+self.addEventListener("install", (event) => {
+  // Create a new cache and add all files to it
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
+
+  event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener("activate", (event) => {
+  // Remove previous cached data from disk
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
+    }
+  }
+
+  event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener("fetch", (event) => {
+  // ignore POST requests etc
+  if (event.request.method !== "GET") return;
+
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
+
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      return cache.match(event.request);
+    }
+
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request);
+
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
+
+      return response;
+    } catch {
+      return cache.match(event.request);
+    }
+  }
+
+  event.respondWith(respond());
+});
