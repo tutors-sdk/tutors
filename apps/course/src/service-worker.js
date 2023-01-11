dd8a1ed7cbd892cbb221e2d87c5a56b1cef2d53b
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-01-11 16:19:52 +0000

    Revert "Update service-worker.js"
    This reverts commit aec22b88a955455c6f2e31da07c598be7ae28b6f.

--- apps/course/src/service-worker.js
+++ apps/course/src/service-worker.js
@@ -18,7 +18,7 @@ const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ["./"];
+const PRECACHE_URLS = ["./", "/_app/immutable/assets"];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
