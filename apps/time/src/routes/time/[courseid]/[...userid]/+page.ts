a3bd1149764fa7d6061853b07151b51b270cae07
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-17 13:06:43 +0000

    fix: test enrollment to make sure it is a string array

--- apps/time/src/routes/time/[courseid]/[...userid]/+page.ts
+++ apps/time/src/routes/time/[courseid]/[...userid]/+page.ts
@@ -7,6 +7,10 @@ import { getKeys } from "../../../../environment";
 import { initFirebase } from "tutors-reader-lib/src/utils/firebase-utils";
 import { currentUser } from "tutors-reader-lib/src/stores/stores";
 
+const isStringArray = (test: any[]): boolean => {
+  return Array.isArray(test) && !test.some((value) => typeof value !== "string");
+};
+
 export const load: PageLoad = async ({ params }) => {
   initFirebase(getKeys().firebase);
   const course: Course = await courseService.readCourse(params.courseid);
@@ -17,11 +21,13 @@ export const load: PageLoad = async ({ params }) => {
   const enrolledUsers: Map<string, UserMetric> = new Map<string, UserMetric>();
   if (course.hasEnrollment()) {
     const students = course.getEnrolledStudentIds();
-    for (const githubId of users.keys()) {
-      if (students.includes(githubId)) {
-        const enrolledUser = users.get(githubId);
-        if (enrolledUser) {
-          enrolledUsers.set(githubId, enrolledUser);
+    if (isStringArray(students)) {
+      for (const githubId of users.keys()) {
+        if (students.includes(githubId)) {
+          const enrolledUser = users.get(githubId);
+          if (enrolledUser) {
+            enrolledUsers.set(githubId, enrolledUser);
+          }
         }
       }
     }
