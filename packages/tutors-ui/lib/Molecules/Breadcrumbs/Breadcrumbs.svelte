b82b270dc5e160352a3c3acd8cc1abd899fe4663
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-02-02 09:03:26 +0000

    chore: prettier

--- packages/tutors-ui/lib/Molecules/Breadcrumbs/Breadcrumbs.svelte
+++ packages/tutors-ui/lib/Molecules/Breadcrumbs/Breadcrumbs.svelte
@@ -44,29 +44,32 @@
 <div class="my-2 mx-8 overflow-hidden p-1">
   <ol class="breadcrumb-nonresponsive text-xs">
     {#if $currentCourse?.lo.properties?.parent != null}
-      <li class="crumb"><a href="/{$currentCourse.lo.properties?.parent}" class="!space-x-[-1rem] lg:!space-x-0">
-        <Icon type="programHome" />
-      </a></li>
+      <li class="crumb">
+        <a href="/{$currentCourse.lo.properties?.parent}" class="!space-x-[-1rem] lg:!space-x-0">
+          <Icon type="programHome" />
+        </a>
+      </li>
 
-	<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+      <li class="crumb-separator" aria-hidden>&rsaquo;</li>
     {/if}
     {#each crumbs($currentLo, []) as lo, i}
-    {#if i >= 1}
-	<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-  {/if}
-    <li class="crumb">
-      <a href="{lo.route}{getUnitId(lo.type, lo.id)}" class="!space-x-[-1rem] lg:!space-x-0 inline-flex !text-black dark:!text-white">
+      {#if i >= 1}
+        <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+      {/if}
+      <li class="crumb">
+        <a href="{lo.route}{getUnitId(lo.type, lo.id)}" class="!space-x-[-1rem] lg:!space-x-0 inline-flex !text-black dark:!text-white">
           <span><Icon type="{lo.type}" /></span>
-        <span
-          class="hidden lg:inline-flex pl-2 items-center"
-          on:mouseenter="{() => {
-            truncated[i] = false;
-          }}"
-          on:mouseleave="{() => {
-            truncated[i] = true;
-          }}">{title(lo.title, truncated[i], i)}</span
-        >
-        </a></li>
+          <span
+            class="hidden lg:inline-flex pl-2 items-center"
+            on:mouseenter="{() => {
+              truncated[i] = false;
+            }}"
+            on:mouseleave="{() => {
+              truncated[i] = true;
+            }}">{title(lo.title, truncated[i], i)}</span
+          >
+        </a>
+      </li>
     {/each}
-    </ol>
+  </ol>
 </div>
