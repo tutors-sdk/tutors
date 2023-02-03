ca5d9c25c57c24b38cd26be59098287d0ee4898e
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-02-03 14:57:36 +0000

    feat: autoplay videos on video page from video card

--- apps/course/src/routes/video/[courseid]/[...loid]/+page.svelte
+++ apps/course/src/routes/video/[courseid]/[...loid]/+page.svelte
@@ -8,7 +8,7 @@
   <div class="min-h-screen flex w-11/12 mx-auto">
     <div class="w-full">
       {#key data.lo}
-        <VideoCard lo="{data.lo}" />
+        <VideoCard lo="{data.lo}" autoplay={true} />
       {/key}
     </div>
     <div class="hidden md:block">
