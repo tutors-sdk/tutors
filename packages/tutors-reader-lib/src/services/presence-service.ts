ca59b47d2c9434fbe34becb3ff9985cb23e6615f
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-02 12:50:34 +0000

    cache user summaries

--- packages/tutors-reader-lib/src/services/presence-service.ts
+++ packages/tutors-reader-lib/src/services/presence-service.ts
@@ -4,13 +4,14 @@ import type { StudentLoEvent, StudentLoUpdate } from "../types/metrics-types";
 import { decrypt, isAuthenticated } from "../utils/auth-utils";
 import { child, get, getDatabase, onValue, ref, off } from "firebase/database";
 import { readObj, readUser, sanitise } from "../utils/firebase-utils";
-import type { User } from "src/types/auth-types";
+import type { User, UserSummary } from "src/types/auth-types";
 
 let canUpdate = false;
 
 export const presenceService = {
   db: {},
   user: <User>{},
+  userSummaryCache: new Map<string, UserSummary>(),
   course: Course,
   lastCourse: Course,
   students: new Map<string, StudentLoEvent>(),
@@ -49,8 +50,11 @@ export const presenceService = {
     if (lo) {
       const userId = decrypt(lo.tutorsTimeId);
       if (userId && this.user.email !== userId) {
-        // const user = await readObj(`${courseId}/users/${sanitise(userId)}`);
-        const user = await readUser(courseId, userId);
+        let user = await readUser(courseId, userId);
+        if (!user) {
+          // const user = await readObj(`${courseId}/users/${sanitise(userId)}`);
+          user = await readUser(courseId, userId);
+        }
         if (user) {
           const event: StudentLoEvent = {
             studentName: user.name,
