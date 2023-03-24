08586d6c9d5d75d1867110d1ee1575fe17f7c90b
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-03-24 10:23:34 +0000

    fix: loading animation on topic

--- apps/course/src/routes/topic/[courseid]/[...loid]/topic.svelte
+++ apps/course/src/routes/topic/[courseid]/[...loid]/topic.svelte
@@ -10,10 +10,12 @@
 {#each topic.panelTalks as lo}
   <TalkCard lo="{lo}" />
   {/each}
-{:else} 
-<ProgressRadial stroke={100} meter="stroke-primary-500" track="stroke-primary-500/30" width="w-20" />
-{/if}
 {#each topic.units as unit}
-  <UnitCard unit="{unit}" />
+<UnitCard unit="{unit}" />
 {/each}
 <CardDeck los="{topic.standardLos}" border />
+{:else} 
+<div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4 flex h-[440px] border-[1px] border-surface-200-700-token">
+  <ProgressRadial stroke="{100}" meter="stroke-primary-500" track="stroke-primary-500/30" width="w-20" class="mt-12 mx-auto" />
+</div>
+{/if}
