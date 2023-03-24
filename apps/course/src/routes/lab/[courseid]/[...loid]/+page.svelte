7a90d0cc8da95d1d833f352b8cce802d6e1783c7
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2023-03-24 10:50:43 +0000

    fix: prose

--- apps/course/src/routes/lab/[courseid]/[...loid]/+page.svelte
+++ apps/course/src/routes/lab/[courseid]/[...loid]/+page.svelte
@@ -64,7 +64,7 @@
   </div>
   <div id="lab-panel" class="w-full min-h-screen">
     <div class="card bg-surface-100-800-token p-8 lg:px-4 py-8 m-2 rounded-xl">
-      <article class="mx-auto prose dark:prose-invert max-w-prose">
+      <article class="mx-auto prose dark:prose-invert w-[90%] lg:max-w-prose xl:max-w-[75%]">
         {@html data.lab.content}
       </article>
     </div>
