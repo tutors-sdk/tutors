8f156bfb28eefae4efb610c5e9ca61fba2fa1f50
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-02 16:14:28 +0100

    feat: add NavTitle

---
+++ packages/tutors-ui/lib/Components/Navigation/NavTitle.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+  import NavTitle from "./NavTitle.svelte";
+</script>
+
+<Meta title="Tutors UI/Navigation" component={NavTitle} />
+
+<Template let:args>
+  <div class="flex">
+    <NavTitle {...args} />
+  </div>
+</Template>
+
+<Story name="NavTitle" args={{image: 'https://picsum.photos/400', title: 'Title Here', subtitle: 'Subtitle Here'}} />
---
+++ packages/tutors-ui/lib/Components/Navigation/NavTitle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Icon from "../../Legacy/Atoms/Icon/Icon.svelte";
+
+  export let image: string | undefined = undefined;
+  export let icon: string | undefined = undefined;
+  export let iconColor: string | undefined = undefined;
+  export let title: string;
+  export let subtitle: string | undefined = undefined;
+</script>
+<div class="w-12">
+  <div class="inline-flex">
+    {#if image}
+      <img src={image} class="w-12 h-12 rounded-md" />
+    {:else if icon}
+      <Icon icon={icon} class="text-{iconColor}" width="40" height="40" />
+    {/if}
+  </div>
+</div>
+<div class="ml-4 flex-nowrap">
+  <h2 class="mr-4 hidden !text-sm font-bold sm:!text-lg md:inline-block">{title}</h2>
+  <div class="hidden md:block">
+      <p class="text-sm font-bold">{subtitle}</p>
+  </div>
+</div>
