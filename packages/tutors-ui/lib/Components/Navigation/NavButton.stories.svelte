b03bfdf75c324570798338603cf71ccf4b687977
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-03 15:05:07 +0100

    feat: navbutton

---
+++ packages/tutors-ui/lib/Components/Navigation/NavButton.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+  import NavButton from "./NavButton.svelte";
+</script>
+
+<Meta title="Components/Navigation" component={NavButton} />
+
+<Template let:args>
+  <div class="flex">
+    <NavButton {...args} />
+  </div>
+</Template>
+
+<Story name="NavButton" args={{href: '#', icon: 'mdi:home', iconColor: 'primary-500', text: 'Home'}} />
