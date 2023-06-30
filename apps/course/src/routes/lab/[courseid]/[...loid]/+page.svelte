a472da9036789eec3d16e3047d142fd3c3d87017
Eamonn de Leastar <edeleastar@wit.ie>
2023-06-30 13:51:21 +0100

    formatting fix

--- apps/course/src/routes/lab/[courseid]/[...loid]/+page.svelte
+++ apps/course/src/routes/lab/[courseid]/[...loid]/+page.svelte
@@ -10,7 +10,7 @@
   });
 
   onDestroy(() => {
-    browser ? window.removeEventListener("keydown", keypressInput) : null
+    browser ? window.removeEventListener("keydown", keypressInput) : null;
   });
 
   afterNavigate(() => {
@@ -47,7 +47,6 @@
   /> -->
 </svelte:head>
 
-
 <div class="block fixed w-full mx-auto bottom-0 lg:hidden bg-primary-50-900-token z-30">
   <nav class="flex flex-wrap justify-between p-2">
     {@html data.lab.horizontalNavbarHtml}
@@ -55,21 +54,22 @@
 </div>
 
 <div class="w-full">
-<div class="flex lg:w-10/12 2xl:w-3/4 mx-auto">
-  <div class="hidden lg:block h-auto w-72 mr-2">
-    <div class="sticky h-auto card bg-surface-100-800-token py-4 m-2 rounded-xl top-6 w-full">
-      <nav class="nav-list">
-        <ul>
-          {@html data.lab.navbarHtml}
-        </ul>
-      </nav>
+  <div class="flex lg:w-10/12 2xl:w-3/4 mx-auto">
+    <div class="hidden lg:block h-auto w-72 mr-2">
+      <div class="sticky h-auto card bg-surface-100-800-token py-4 m-2 rounded-xl top-6 w-full">
+        <nav class="nav-list">
+          <ul>
+            {@html data.lab.navbarHtml}
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div id="lab-panel" class="flex-1 w-1/2 min-h-screen">
+      <div class="card bg-surface-100-800-token p-8 lg:px-4 py-8 m-2 rounded-xl">
+        <article class="mx-auto prose dark:prose-invert max-w-none w-[80%]">
+          {@html data.lab.content}
+        </article>
+      </div>
     </div>
   </div>
-  <div id="lab-panel" class="flex-1 w-1/2 min-h-screen">
-    <div class="card bg-surface-100-800-token p-8 lg:px-4 py-8 m-2 rounded-xl">
-      <article class="mx-auto prose dark:prose-invert max-w-none w-[80%]">
-        {@html data.lab.content}
-      </article>
-    </div>
-  </div>
-</div></div>
+</div>
