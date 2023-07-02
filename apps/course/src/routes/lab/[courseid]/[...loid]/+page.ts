1996322653027aaaf49f7374fc0c359007243c0f
Eamonn de Leastar <edeleastar@wit.ie>
2023-07-02 09:24:02 +0100

    fix: remove markdown init

--- apps/course/src/routes/lab/[courseid]/[...loid]/+page.ts
+++ apps/course/src/routes/lab/[courseid]/[...loid]/+page.ts
@@ -5,7 +5,7 @@ import { courseService } from "tutors-reader-lib/src/services/course-service";
 export const ssr = false;
 
 export const load: PageLoad = async ({ url, params }) => {
-  initMarkdownParser();
+  // initMarkdownParser();
   const lab = await courseService.readLab(params.courseid, url.pathname);
   const lastSegment = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
   if (lastSegment.startsWith("book")) {
