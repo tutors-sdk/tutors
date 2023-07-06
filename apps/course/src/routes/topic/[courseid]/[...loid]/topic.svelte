5ef73b990c56cc4ae71e4e46a73324225c7e1d27
Eamonn de Leastar <edeleastar@wit.ie>
2023-07-06 13:02:55 +0100

    fix: note card support + added web resources to walls

--- apps/course/src/routes/topic/[courseid]/[...loid]/topic.svelte
+++ apps/course/src/routes/topic/[courseid]/[...loid]/topic.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
+  import { browser } from "$app/environment";
   import type { Topic } from "tutors-reader-lib/src/models/topic";
-  import { CardDeck, UnitCard, TalkCard } from "tutors-ui";
+  import { CardDeck, UnitCard, TalkCard, NoteCard } from "tutors-ui";
   import { ProgressRadial } from "@skeletonlabs/skeleton";
   export let topic: Topic;
 </script>
 
 {#if browser}
-{#each topic.panelTalks as lo}
-  <TalkCard lo="{lo}" />
+  {#each topic.panelTalks as lo}
+    <TalkCard lo="{lo}" />
   {/each}
-{#each topic.units as unit}
-<UnitCard unit="{unit}" />
-{/each}
-<CardDeck los="{topic.standardLos}" border />
-{:else} 
-<div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4 flex h-[440px] border-[1px] border-surface-200-700-token">
-  <ProgressRadial stroke="{100}" meter="stroke-primary-500" track="stroke-primary-500/30" width="w-20" class="mt-12 mx-auto" />
-</div>
+  {#each topic.panelNotes as lo}
+    <NoteCard lo="{lo}" />
+  {/each}
+  {#each topic.units as unit}
+    <UnitCard unit="{unit}" />
+  {/each}
+  <CardDeck los="{topic.standardLos}" border />
+{:else}
+  <div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4 flex h-[440px] border-[1px] border-surface-200-700-token">
+    <ProgressRadial stroke="{100}" meter="stroke-primary-500" track="stroke-primary-500/30" width="w-20" class="mt-12 mx-auto" />
+  </div>
 {/if}
