3d4a08ef65898e3d32e3562c02b72872b0d05612
Eamonn de Leastar <edeleastar@wit.ie>
2023-07-20 11:49:53 +0100

    refactor: new TopDeck to unify top level card deck display

--- packages/tutors-ui/lib/Organisms/CardDeck/CardDeck.svelte
+++ packages/tutors-ui/lib/Organisms/CardDeck/CardDeck.svelte
@@ -16,15 +16,13 @@
   let unbordered = "";
 </script>
 
-{#if los.length}
-  <div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4 {border ? bordered : unbordered}">
-    <div class="flex flex-wrap justify-center">
-      {#each orderedLos as lo}
-        <Card lo="{lo}" />
-      {/each}
-      {#each unOrderedLos as lo}
-        <Card lo="{lo}" />
-      {/each}
-    </div>
+<div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4 {border ? bordered : unbordered}">
+  <div class="flex flex-wrap justify-center">
+    {#each orderedLos as lo}
+      <Card lo="{lo}" />
+    {/each}
+    {#each unOrderedLos as lo}
+      <Card lo="{lo}" />
+    {/each}
   </div>
-{/if}
+</div>
