14f028a06ba034119cba3e2dd01742c7cd4fd3c6
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-07-29 14:52:13 +0100

    fix: prettier and config files

--- apps/course/svelte.config.js
+++ apps/course/svelte.config.js
@@ -1,19 +1,19 @@
-import adapter from "@sveltejs/adapter-netlify";
-import preprocess from "svelte-preprocess";
-
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter()
-  }
+	vitePlugin: {
+		inspector: true
+	},
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
-
 export default config;
