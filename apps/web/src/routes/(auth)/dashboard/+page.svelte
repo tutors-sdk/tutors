42a8c3b65f26c055f2e288ab35290ea618c43456
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-07-31 15:01:55 +0100

    fix: dashboard

--- apps/web/src/routes/(auth)/dashboard/+page.svelte
+++ apps/web/src/routes/(auth)/dashboard/+page.svelte
@@ -4,19 +4,19 @@
 	export let data;
 </script>
 
-<div class="container mx-auto">
-	<div class="card m-8 p-8">
-		<div class="table-container">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>Course Name</th>
-						<th>Last Accessed</th>
-						<th>No Of Visits</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>
+<div class="container mx-auto p-8">
+	<div class="table-container">
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Course Name</th>
+					<th>Last Accessed</th>
+					<th>No Of Visits</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				{#if data.courses}
 					{#each data.courses[0].course_list.courses as course}
 						<tr>
 							<td>{course.name}</td>
@@ -25,8 +25,10 @@
 							<td />
 						</tr>
 					{/each}
-				</tbody>
-			</table>
-		</div>
+				{:else}
+					Oops, looks like you haven't visited any courses yet!
+				{/if}
+			</tbody>
+		</table>
 	</div>
 </div>
