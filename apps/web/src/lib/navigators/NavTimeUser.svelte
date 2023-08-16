3e5ee438b73808979c96b3697301ea4cd6657dcc
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-16 11:37:52 +0100

    fix: moar imports

--- apps/web/src/lib/navigators/NavTimeUser.svelte
+++ apps/web/src/lib/navigators/NavTimeUser.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-  import { currentCourse, currentUser } from "tutors-reader-lib/src/stores/stores";
-  import { Icon } from "tutors-ui";
-  import { popup, Avatar } from "@skeletonlabs/skeleton";
-  let gitUrl = "";
+	import { currentCourse, currentUser } from '$lib/stores';
+	import { Icon } from '$lib/ui/legacy';
+	import { popup, Avatar } from '@skeletonlabs/skeleton';
+	let gitUrl = '';
 </script>
 
 {#if $currentUser && $currentCourse.authLevel > 0}
-  <div class="relative">
-    <button class="btn btn-sm space-x-1" use:popup="{{ event: 'click', target: 'avatar' }}">
-      <div class="relative inline-block">
-        <Avatar width="w-10" src="{$currentUser.picture}" alt="{$currentUser?.nickname}" />
-      </div>
-    </button>
-    <nav class="list-nav card card-body w-56 space-y-4 shadow-lg" data-popup="avatar">
-      <span class="mt-2 ml-4 text-xs">Logged in as:</span><br />
-      <span class="ml-4 text-sm">{$currentUser.name}</span>
-      <hr />
-      <ul>
-        <li>
-          <a href="{gitUrl}" target="_blank" rel="noreferrer">
-            <Icon type="github" />
-            <div class="ml-2">Github Profile</div>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+	<div class="relative">
+		<button class="btn btn-sm space-x-1" use:popup={{ event: 'click', target: 'avatar' }}>
+			<div class="relative inline-block">
+				<Avatar width="w-10" src={$currentUser.picture} alt={$currentUser?.nickname} />
+			</div>
+		</button>
+		<nav class="list-nav card card-body w-56 space-y-4 shadow-lg" data-popup="avatar">
+			<span class="mt-2 ml-4 text-xs">Logged in as:</span><br />
+			<span class="ml-4 text-sm">{$currentUser.name}</span>
+			<hr />
+			<ul>
+				<li>
+					<a href={gitUrl} target="_blank" rel="noreferrer">
+						<Icon type="github" />
+						<div class="ml-2">Github Profile</div>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
 {/if}
