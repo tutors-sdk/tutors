c34e60e383422318eb9aa4734a49f8490a58d942
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-17 10:21:52 +0100

    feat(breaking): remove tutors-lib

--- packages/tutors-lib/src/utils/properties.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Properties = void 0;
-class Properties {
-}
-exports.Properties = Properties;
-//# sourceMappingURL=properties.js.map
