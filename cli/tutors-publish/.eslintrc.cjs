05bef03fdedb9f8873882c52c183dce0ff81bf29
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-18 12:38:39 +0100

    feat: ci integration

--- cli/tutors-publish/.eslintrc.cjs
+++ cli/tutors-publish/.eslintrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ["custom"],
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "turbo", "prettier"],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
 };
