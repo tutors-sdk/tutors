7c3917e47fa01e83afd779ac1f733cdb1e05ceca
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-22 21:18:46 +0100

    feat: primary nav user menu

--- app/src/lib/components/index.ts
+++ app/src/lib/components/index.ts
@@ -3,3 +3,4 @@ export { NavigationPrimary } from "./Navigation/Primary";
 export { NavigationPrimaryTitle } from "./Navigation/Primary/Title";
 export { NavigationPrimaryButton } from "./Navigation/Primary/Button";
 export { NavigationPrimaryUser } from "./Navigation/Primary/User";
+export { NavigationPrimaryUserMenu } from "./Navigation/Primary/UserMenu";
