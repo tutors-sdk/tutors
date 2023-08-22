c088a5e2db10fc12695ef26cb33e9928e5674030
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-22 15:58:57 +0100

    feat: Nav primary link

---
+++ app/src/lib/components/Navigation/Primary/Link/NavigationPrimaryLink.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import NavigationPrimaryLink from "./NavigationPrimaryLink.svelte";
+</script>
+
+<Meta title="Tutors/Navigation/Primary/Link" component={NavigationPrimaryLink} />
+
+<Template let:args>
+  <NavigationPrimaryLink {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    href: "#",
+    label: "Your Label"
+  }}
+/>
---
+++ app/src/lib/components/Navigation/Primary/Link/NavigationPrimaryLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string = "";
+  export let label: string = "";
+</script>
+
+<a class="btn btn-sm" {href}>
+  <span class="text-sm font-bold block">{label}</span>
+</a>
