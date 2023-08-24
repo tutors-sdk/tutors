bca1586106cbbb968866b0916a6061657df9bd4a
Eamonn de Leastar <edeleastar@wit.ie>
2023-08-24 08:10:14 +0100

    chore: remove generated js files

--- cli/tutors-time/src/filrebase-utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function initFirebase(keys: any): Promise<void>;
-export declare function deleteNode(root: string, url: string): Promise<void>;
-export declare function readValue(key: string): Promise<string>;
-export declare function readVisits(courseId: string): Promise<number | undefined>;
-export declare function readAllCourseIds(keys: any, root: string): Promise<string[]>;
-export declare function isValidCourseName(course: string): boolean;
