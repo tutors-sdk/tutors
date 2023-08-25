59b8e7c3ca0a3d7889854352742e4bde870ca091
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-25 13:09:24 +0100

    test: test

--- app/cypress/e2e/tutorsReader.cy.ts
+++ app/cypress/e2e/tutorsReader.cy.ts
@@ -12,7 +12,7 @@ describe("Loading the JSON fixture", function () {
 
   beforeEach("Check: Define the dimensions of the screen being used", function () {
     //This defines the dimensions of the screen
-    cy.visit(`${course.route}course/tutors-cypress-testing`);
+    cy.visit(`${course.route}`);
     cy.viewport(1440, 1440);
   });
   /**
