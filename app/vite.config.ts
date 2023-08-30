ef5fa3fc7135fb6477b25b26fbd5da5bb56a2b95
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-08-30 14:41:10 +0100

    fix: remove purgecss blocking build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [
-    sveltekit(),
-    purgeCss({
-      safelist: {
-        greedy: [/^hljs-/]
-      }
-    })
-  ]
+  plugins: [sveltekit()]
 };
 
 export default config;
