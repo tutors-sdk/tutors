e4dcd19d0343305a5f1aa1317448c80a9e803b19
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-09-01 16:22:32 +0100

    feat: add course accordion

--- app/src/routes/(auth)/dashboard/+page.svelte
+++ app/src/routes/(auth)/dashboard/+page.svelte
@@ -2,6 +2,8 @@
   import { courseService } from "$lib/services/course";
   import type { Course } from "$lib/services/models/course";
   import {
+    Accordion,
+    AccordionItem,
     getToastStore,
     getModalStore,
     ProgressRadial,
@@ -99,58 +101,57 @@
   };
 
   const handleDeleteCourse = async (courseId) => {
-  let loading = true;
+    let loading = true;
 
-  const modal: ModalSettings = {
-    type: "confirm",
-    // Data
-    title: "Please Confirm",
-    body: "Are you sure you want to delete the course?",
-    response: async (r: boolean) => {
-      if (r === true) {
-        try {
-          const { data: userCourseList, error } = await data.supabase
-            .from("accessed_courses")
-            .select(`course_list`)
-            .eq("id", data.session.user.id);
-
-          if (error) {
-            showErrorMessage("Error fetching user course list");
-            return;
-          }
-
-          const courseList = userCourseList[0].course_list;
-
-          const courseIndex = courseList.courses.findIndex((course) => course.id === courseId);
-
-          if (courseIndex !== -1) {
-            courseList.courses.splice(courseIndex, 1);
-            const { error } = await data.supabase
+    const modal: ModalSettings = {
+      type: "confirm",
+      // Data
+      title: "Please Confirm",
+      body: "Are you sure you want to delete the course?",
+      response: async (r: boolean) => {
+        if (r === true) {
+          try {
+            const { data: userCourseList, error } = await data.supabase
               .from("accessed_courses")
-              .update({ course_list: courseList })
+              .select(`course_list`)
               .eq("id", data.session.user.id);
 
             if (error) {
-              showErrorMessage("Error deleting course");
-            } else {
-              showSuccessMessage("Course deleted successfully");
-              displayedCourseList = courseList.courses;
+              showErrorMessage("Error fetching user course list");
+              return;
             }
-          } else {
-            showErrorMessage("Course not found in your list");
+
+            const courseList = userCourseList[0].course_list;
+
+            const courseIndex = courseList.courses.findIndex((course) => course.id === courseId);
+
+            if (courseIndex !== -1) {
+              courseList.courses.splice(courseIndex, 1);
+              const { error } = await data.supabase
+                .from("accessed_courses")
+                .update({ course_list: courseList })
+                .eq("id", data.session.user.id);
+
+              if (error) {
+                showErrorMessage("Error deleting course");
+              } else {
+                showSuccessMessage("Course deleted successfully");
+                displayedCourseList = courseList.courses;
+              }
+            } else {
+              showErrorMessage("Course not found in your list");
+            }
+          } catch (e) {
+            showErrorMessage("An error occurred while deleting the course");
+          } finally {
+            loading = false;
           }
-        } catch (e) {
-          showErrorMessage("An error occurred while deleting the course");
-        } finally {
-          loading = false;
         }
       }
-    }
+    };
+
+    modalStore.trigger(modal);
   };
-
-  modalStore.trigger(modal);
-};
-
 
   function showErrorMessage(message: string) {
     const t: ToastSettings = {
@@ -176,23 +177,33 @@
 </div>
 
 <div class="container card mx-auto p-8 my-4">
-  <p class="text-2xl font-bold pb-4">Add a course</p>
-  <form on:submit|preventDefault={handleAddCourse} bind:this={addCourseForm}>
-    <div class="grid grid-cols-4">
-      <input
-        class="input col-span-3"
-        title="Input (text)"
-        type="text"
-        placeholder="Insert course ID here"
-        bind:value={newCourseInput}
-      />
-      {#if loading}
-        <button class="btn variant-filled-primary ml-4"><ProgressRadial width="w-6" /></button>
-      {:else}
-        <input class="btn variant-filled-primary ml-4" type="submit" value="Add" />
-      {/if}
-    </div>
-  </form>
+  <Accordion>
+    <AccordionItem>
+      <svelte:fragment slot="summary">
+        <p class="text-2xl font-bold">Add a course</p>
+      </svelte:fragment>
+      <svelte:fragment slot="content">
+        <form on:submit|preventDefault={handleAddCourse} bind:this={addCourseForm}>
+          <div class="grid grid-cols-4">
+            <input
+              class="input col-span-3"
+              title="Input (text)"
+              type="text"
+              placeholder="Insert course ID here"
+              bind:value={newCourseInput}
+            />
+            {#if loading}
+              <button class="btn variant-filled-primary ml-4">
+                <ProgressRadial width="w-6" />
+              </button>
+            {:else}
+              <input class="btn variant-filled-primary ml-4" type="submit" value="Add" />
+            {/if}
+          </div>
+        </form>
+      </svelte:fragment>
+    </AccordionItem>
+  </Accordion>
 </div>
 
 <div class="container card mx-auto p-8">
