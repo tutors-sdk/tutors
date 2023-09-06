71b29ed65efde293887f322a63070901c3b82151
Eamonn de Leastar <edeleastar@wit.ie>
2023-09-06 09:14:32 +0100

    fix: correct types for units

--- app/src/lib/ui/legacy/Organisms/CardDeck/UnitDeck.svelte
+++ app/src/lib/ui/legacy/Organisms/CardDeck/UnitDeck.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { UnitCard } from "$lib/ui/legacy";
-  import type { Unit } from "$lib/services/models/lo-types";
+  import type { Unit, Lo } from "$lib/services/models/lo-types";
   export let units: Unit[];
 </script>
 
