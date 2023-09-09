0a4d35078871c96fc7b2e23d807ec5f8d9a536f1
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-09-09 19:39:02 +0100

    fix: prettier

--- app/src/routes/terms/+page.svelte
+++ app/src/routes/terms/+page.svelte
@@ -4,35 +4,19 @@
 
     <div class="font-sm text-l p-2 text-left font-light">
       <p class="p-2">
-        You are authenticated via your social login credentials to Tutors. Tutors will record how
-        much time each view is active and send this to the TutorsTime data store. You can view this
-        information via the TutorsTime feature on the course Navigator.<br /><br />
-        This data is available to you + the instructor of your course, but not to other students. No
-        other data is gathered, nor is this data transmitted anywhere other than the TutorsTime data
-        store.<br /><br />
-        TutorsLive also uses this data to display of panel of students currently online. You can opt
-        out of this feature from the TutorsLive page.
+        You are authenticated via your social login credentials to Tutors. Tutors will record how much time each view is active and send this to the TutorsTime data store. You can
+        view this information via the TutorsTime feature on the course Navigator.<br /><br />
+        This data is available to you + the instructor of your course, but not to other students. No other data is gathered, nor is this data transmitted anywhere other than the TutorsTime
+        data store.<br /><br />
+        TutorsLive also uses this data to display of panel of students currently online. You can opt out of this feature from the TutorsLive page.
       </p>
       <p class="p-2">To learn more about these features please consult:</p>
-      <ul
-        class="ml-4 list-inside list-disc text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
-      >
+      <ul class="ml-4 list-inside list-disc text-blue-600 underline visited:text-purple-600 hover:text-blue-800">
         <li>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/tutors-sdk/tutors-faqs/blob/main/tutors-time-faq.md"
-          >
-            Tutors Time FAQ
-          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/tutors-sdk/tutors-faqs/blob/main/tutors-time-faq.md"> Tutors Time FAQ </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/tutors-sdk/tutors-faqs/blob/main/tutors-live-faq.md"
-            >Tutors Live FAQ
-          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/tutors-sdk/tutors-faqs/blob/main/tutors-live-faq.md">Tutors Live FAQ </a>
         </li>
       </ul>
       <p class="p-2">
