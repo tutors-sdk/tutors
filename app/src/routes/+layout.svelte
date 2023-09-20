03d9db1de3294052ac88f51197e3103a5d77ca2d
Eamonn de Leastar <edeleastar@wit.ie>
2023-09-20 07:20:56 +0100

    fix: reintroduce 'live' and 'catalogue' views

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { getKeys } from "$lib/environment";
+  import { initFirebase } from "$lib/services/utils/firebase";
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
   import { initializeStores, storePopup } from "@skeletonlabs/skeleton";
   initializeStores();
   const themes: any = ["tutors", "dyslexia", "halloween", "valentines"];
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+  initFirebase(getKeys().firebase);
 </script>
 
 <slot />
