3becb4e7b6196d2856d10730bdb65092e9f44845
Eamonn de Leastar <edeleastar@wit.ie>
2023-09-27 10:53:47 +0100

    fix: include yaml files in permissible asset types for archives in labs

--- cli/tutors-gen-lib/src/models/lo-types.ts
+++ cli/tutors-gen-lib/src/models/lo-types.ts
@@ -1,5 +1,5 @@
 export const imageTypes = ["png", "jpg", "jpeg", "gif", "PNG", "JPG", "JPEG", "GIF"];
-export const assetTypes = imageTypes.concat(["pdf", "zip", "html", "htm"]);
+export const assetTypes = imageTypes.concat(["pdf", "zip", "html", "htm", "yaml", "txt"]);
 
 export type WeekType = {
   title: string;
--- cli/tutors-gen-lib/src/tutors.ts
+++ cli/tutors-gen-lib/src/tutors.ts
@@ -5,7 +5,7 @@ import { resourceBuilder } from "./generator/resource-builder";
 import { writeFile } from "./generator/file-utils";
 import { generateNetlifyToml } from "./generator/netlify";
 
-export const version = "3.2.4";
+export const version = "3.2.5";
 
 export function parseCourse(folder: string): Course {
   resourceBuilder.buildTree(folder);
