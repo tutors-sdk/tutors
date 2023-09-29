b96e4b2e28f57f5ff3097f6967779968f68adf7a
Eamonn de Leastar <edeleastar@wit.ie>
2023-09-29 07:59:51 +0100

    refactor: move buttons to separate folder

---
+++ app/src/lib/ui/navigators/buttons/TutorsTimeIndicator.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { currentCourse, onlineStatus } from "$lib/stores";
+  import Icon from "$lib/ui/icons/Icon.svelte";
+</script>
+
+{#if $currentCourse.authLevel > 0 && $onlineStatus}
+  <Icon icon="eos-icons:hourglass" tip={"This course is TutorsTime enabled"} color={"green"} />
+{/if}
