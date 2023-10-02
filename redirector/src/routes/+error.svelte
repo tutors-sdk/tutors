eaba467e6f9be2ad6da55d60a38d01a6ce449b29
Eamonn de Leastar <edeleastar@wit.ie>
2023-10-02 14:16:55 +0100

    first draft of redirector

---
+++ redirector/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { page } from "$app/stores";
+  const redirectTo = `https://tutors.dev${$page.url.pathname}`;
+  if (browser) window.location = redirectTo;
+</script>
+
+<pre>Redirecting to: {redirectTo} </pre>
