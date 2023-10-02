9b779241de7e7debe8b3165bac7b17487ccb748a
Eamonn de Leastar <edeleastar@wit.ie>
2023-10-02 17:51:05 +0100

    feature: wildcard path to implement redirect

--- redirector/src/routes/[...coursepath]/+page.svelte
+++ redirector/src/routes/[...coursepath]/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  import { browser } from "$app/environment";
   import { page } from "$app/stores";
   const redirectTo = `https://tutors.dev${$page.url.pathname}`;
-  if (browser) window.location = redirectTo;
+  window.location = redirectTo;
 </script>
 
-<pre>Redirecting to: {redirectTo} </pre>
+<pre> Redirecting to: {redirectTo} </pre>
