77cd9572680cc0a907f90748025b1a12b76b2f2f
Eamonn de Leastar <edeleastar@wit.ie>
2023-10-16 09:34:22 +0100

    feature: include pkt in static files list

--- cli/tutors-gen-lib/src/models/lo-types.ts
+++ cli/tutors-gen-lib/src/models/lo-types.ts
@@ -1,5 +1,5 @@
 export const imageTypes = ["png", "jpg", "jpeg", "gif", "PNG", "JPG", "JPEG", "GIF"];
-export const assetTypes = imageTypes.concat(["pdf", "zip", "html", "htm", "yaml", "xls", "xlsx", "xlsm", "csv"]);
+export const assetTypes = imageTypes.concat(["pdf", "zip", "html", "htm", "yaml", "xls", "xlsx", "xlsm", "csv", "pkt"]);
 
 export type WeekType = {
   title: string;
