0a99c2d664d7e119b0e2c7c588d893f6c7240faa
Eamonn de Leastar <edeleastar@wit.ie>
2023-11-01 08:34:30 +0000

    send currentLo to partykit server

---
+++ app/src/lib/services/party-kit.ts
@@ -0,0 +1,71 @@
+import PartySocket from "partysocket";
+import type { Course, IconType, Lo } from "./models/lo-types";
+import type { User } from "./types/auth";
+
+export interface LoUser {
+  fullName: string;
+  avatar: string;
+  id: string;
+}
+
+export interface LoEvent {
+  courseId: string;
+  courseUrl: string;
+  courseTitle: string;
+  loRoute: string;
+  title: string;
+  img?: string;
+  icon?: IconType;
+  isPrivate: boolean;
+  user: LoUser;
+}
+
+const partyKit = new PartySocket({
+  host: "https://tutors-party.edeleastar.partykit.dev",
+  room: "tutors-all-course-access"
+});
+
+function getUser(onlineStatus: boolean, userDetails: User): LoUser {
+  const user: LoUser = {
+    fullName: "anonymous",
+    avatar: "none",
+    id: getTutorsTimeId()
+  };
+  if (userDetails && onlineStatus) {
+    user.fullName = userDetails.user_metadata.full_name ? userDetails.user_metadata.full_name : userDetails.user_metadata.user_name;
+    (user.avatar = userDetails.user_metadata.avatar_url), (user.id = userDetails.user_metadata.user_name);
+  }
+  return user;
+}
+
+export function sendLoEvent(course: Course, currentLo: Lo, onlineStatus: boolean, userDetails: User) {
+  const lo: LoEvent = {
+    courseId: course.courseId,
+    courseUrl: course.courseUrl,
+    img: currentLo.img,
+    title: currentLo.title,
+    courseTitle: course.title,
+    loRoute: currentLo.route,
+    user: getUser(onlineStatus, userDetails),
+    isPrivate: (course.properties?.private as unknown as number) === 1
+  };
+  if (currentLo.icon) {
+    lo.icon = currentLo.icon;
+  }
+  partyKit.send(JSON.stringify(lo));
+}
+
+function generateTutorsTimeId() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+function getTutorsTimeId() {
+  if (!window.localStorage.tutorsTimeId) {
+    window.localStorage.tutorsTimeId = generateTutorsTimeId();
+  }
+  return window.localStorage.tutorsTimeId;
+}
