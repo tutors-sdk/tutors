736fa9389f35ede498b4b5bfdff35b4091276bca
Eamonn de Leastar <edeleastar@wit.ie>
2023-11-03 08:10:00 +0000

    feature: active part implementation

--- app/src/routes/(time)/active-party/[courseid]/+page.ts
+++ app/src/routes/(time)/active-party/[courseid]/+page.ts
@@ -1,11 +1,8 @@
-import { getKeys } from "$lib/environment";
 import { courseService } from "$lib/services/course";
 import type { Course } from "$lib/services/models/lo-types";
-import { initFirebase } from "$lib/services/utils/firebase";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  initFirebase(getKeys().firebase);
   const course: Course = await courseService.readCourse(params.courseid, fetch);
   return {
     course: course
