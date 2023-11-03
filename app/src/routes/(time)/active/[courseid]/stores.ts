c00be2e2366586e53dd5a3285186915272a851df
Eamonn de Leastar <edeleastar@wit.ie>
2023-11-03 08:24:09 +0000

    replace firebase presence with partykit implementation

--- app/src/routes/(time)/active/[courseid]/stores.ts
+++
@@ -1,6 +0,0 @@
-import type { StudentLoEvent } from "$lib/services/types/metrics";
-import { writable } from "svelte/store";
-
-const students: StudentLoEvent[] = [];
-export const studentsOnline = writable(0);
-export const studentsOnlineList = writable(students);
--- app/src/routes/(time)/live/+page.svelte
+++ app/src/routes/(time)/live/+page.svelte
@@ -1,103 +1,41 @@
 <script lang="ts">
   import "../../../app.postcss";
-  import { onMount } from "svelte";
-  import { child, get, getDatabase, onValue, ref } from "firebase/database";
-  import { getCourseSummary, type CourseSummary } from "$lib/services/utils/all-course-access";
-  import Metric from "$lib/ui/icons/Metric.svelte";
-  import LiveCourseCard from "./LiveCourseCard.svelte";
+  import PartySocket from "partysocket";
+  import LiveCourseCard from "$lib/ui/time/LiveCourseCard.svelte";
   import TutorsShell from "$lib/ui/app-shells/TutorsShell.svelte";
-  const db = getDatabase();
+  import { refreshLoEvent, type LoEvent } from "$lib/services/party-kit";
+
+  const partyKit = new PartySocket({
+    host: "https://tutors-party.edeleastar.partykit.dev",
+    room: "tutors-all-course-access"
+  });
 
   export let data: any;
   let { supabase, session } = data;
   $: ({ supabase, session } = data);
 
-  let los: CourseSummary[] = [];
-  let courses = new Map<string, CourseSummary>();
-  let modules = 0;
-  let subTitle = "Connecting to Tutors Store...";
-  let activeSince = "";
-  let visits = 0;
-  const tutorsTimeIds = new Set<string>();
-  let users = 0;
+  let los: LoEvent[] = [];
+  const loEventMap = new Map<string, LoEvent>();
 
-  let countDown = 10;
-  let canUpdate = false;
-  let timer = setInterval(function () {
-    activeSince = new Date().toLocaleTimeString();
-    subTitle = `Please standby, Will be connected in ${countDown} seconds`;
-    countDown--;
-    if (countDown == 0) {
-      clearInterval(timer);
-      activeSince = new Date().toLocaleTimeString();
-      subTitle = `Connected at : ${activeSince}`;
-      canUpdate = true;
-    }
-  }, 1000);
-
-  function updateNmrUsers(courseSummary: CourseSummary) {
-    if (courseSummary.currentLo.user.id) {
-      tutorsTimeIds.add(courseSummary.currentLo.user.id);
-      courseSummary.studentIds.add(courseSummary.currentLo.user.id);
-      users = tutorsTimeIds.size;
-    }
-  }
-
-  function updateCourseSummary(courseSummary: CourseSummary) {
-    courseSummary.currentLo.route = `https://reader.tutors.dev${courseSummary.currentLo?.subRoute}`;
-    courseSummary.img = courseSummary.currentLo?.img;
-    if (courseSummary.currentLo.icon) {
-      courseSummary.icon = courseSummary.currentLo.icon;
-    } else {
-      courseSummary.icon = null;
-    }
-  }
-
-  async function visitUpdate(courseId: string) {
-    let courseSummary = courses.get(courseId);
-    if (!courseSummary) {
-      courseSummary = await getCourseSummary(courseId);
-      if (courseSummary.isPrivate) return;
-      courses.set(courseId, courseSummary);
-      los.push(courseSummary);
-    }
-    const usage = await (await get(child(ref(db), `all-course-access/${courseId}`))).val();
-    if (usage.learningEvent) {
-      courseSummary.currentLo = usage.learningEvent;
-      courseSummary.visits = usage.visits;
-      updateCourseSummary(courseSummary);
-      updateNmrUsers(courseSummary);
+  partyKit.addEventListener("message", (event) => {
+    try {
+      const nextLoEvent = JSON.parse(event.data);
+      let loEvent = loEventMap.get(nextLoEvent.courseId);
+      if (!loEvent) {
+        los.push(nextLoEvent);
+        loEventMap.set(nextLoEvent.courseId, nextLoEvent);
+      } else {
+        refreshLoEvent(loEvent, nextLoEvent);
+      }
       los = [...los];
-      modules = los.length;
-    }
-  }
-
-  async function startListening(allCourses: any) {
-    allCourses.forEach((courseAccess: any) => {
-      const courseId = courseAccess.courseId;
-      const statusRef = ref(db, `all-course-access/${courseId}/visits`);
-      onValue(statusRef, async () => {
-        if (canUpdate) {
-          await visitUpdate(courseId);
-          visits++;
-        }
-      });
-    });
-  }
-
-  onMount(async () => {
-    if (data.allCourses) {
-      startListening(data.allCourses);
+    } catch (e) {
+      console.log(e);
     }
   });
 </script>
 
-<TutorsShell {session} {supabase} title="Tutors Live Stream" {subTitle}>
-  <div slot="header" class="hidden md:inline-block">
-    <Metric value={modules} title="Active Modules" />
-    <Metric value={visits} title="Page Loads" />
-    <Metric value={users} title="Students Online" />
-  </div>
+<TutorsShell {session} {supabase} title="Tutors Live Stream">
+  <div slot="header" class="hidden md:inline-block"></div>
   <div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4">
     <div class="flex flex-wrap justify-center">
       {#each los as lo}
