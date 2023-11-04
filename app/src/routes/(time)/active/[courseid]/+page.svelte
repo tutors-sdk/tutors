bc7d95237f5323fde79645c3af44100a774116d3
Eamonn de Leastar <edeleastar@wit.ie>
2023-11-04 13:37:34 +0000

    refactor: reimplement live and active using partykit presence service

--- app/src/routes/(time)/active/[courseid]/+page.svelte
+++ app/src/routes/(time)/active/[courseid]/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import "../../../../app.postcss";
   import { beforeUpdate, onMount } from "svelte";
-  import PartySocket from "partysocket";
-  import { refreshLoEvent, type LoEvent } from "$lib/services/party-kit";
-  import StudentCard from "$lib/ui/time/StudentCard.svelte";
+  import { startPresenceService } from "$lib/services/presence";
   import TutorsShell from "$lib/ui/app-shells/TutorsShell.svelte";
   import { goto } from "$app/navigation";
+  import CoursePresence from "$lib/ui/time/CoursePresence.svelte";
 
   export let data: any;
   let { supabase, session } = data;
@@ -17,40 +16,13 @@
     }
   });
 
-  const loEventMap = new Map<string, LoEvent>();
-  let los: LoEvent[] = [];
-
   onMount(async () => {
-    const partyKitCourse = new PartySocket({
-      host: "https://tutors-party.edeleastar.partykit.dev",
-      room: data.course.courseId
-    });
-    partyKitCourse.addEventListener("message", (event) => {
-      try {
-        const nextLoEvent = JSON.parse(event.data);
-        let loEvent = loEventMap.get(nextLoEvent.user.id);
-        if (!loEvent) {
-          los.push(nextLoEvent);
-          loEventMap.set(nextLoEvent.user.id, nextLoEvent);
-        } else {
-          refreshLoEvent(loEvent, nextLoEvent);
-        }
-        los = [...los];
-      } catch (e) {
-        console.log(e);
-      }
-    });
+    startPresenceService(data.course);
   });
 </script>
 
 <TutorsShell subTitle={data.course.title} {supabase} {session}>
   <slot>
-    <div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4">
-      <div class="flex flex-wrap justify-center">
-        {#each los as lo}
-          <StudentCard {lo} />
-        {/each}
-      </div>
-    </div>
+    <CoursePresence />
   </slot>
 </TutorsShell>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -5,9 +5,11 @@
   import { initFirebase } from "$lib/services/utils/firebase";
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
   import { initializeStores, storePopup } from "@skeletonlabs/skeleton";
+
   initializeStores();
   const themes: any = ["tutors", "dyslexia", "halloween", "valentines"];
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
   if (getKeys().firebase.apiKey !== "XXX") {
     initFirebase(getKeys().firebase);
   }
