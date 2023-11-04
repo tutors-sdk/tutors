68492c7b137130c6ad1002f16df97999961f9484
Eamonn de Leastar <edeleastar@wit.ie>
2023-11-04 13:35:25 +0000

    refactor: use partykit based presence types

--- app/src/lib/stores.ts
+++ app/src/lib/stores.ts
@@ -1,9 +1,8 @@
 import { writable, type Writable } from "svelte/store";
-import type { StudentLoEvent } from "$lib/services/types/metrics";
 import type { User } from "$lib/services/types/auth";
 import { localStorageStore } from "@skeletonlabs/skeleton";
 import type { Lo, Course } from "$lib/services/models/lo-types";
-import type { PresenceObject } from "./services/types/presence";
+import type { LoEvent } from "./services/types/presence";
 
 export const revealSidebar = writable(false);
 export const revealOnline = writable(false);
@@ -18,6 +17,5 @@ export const storePreview = localStorageStore("storePreview", false);
 export const onlineStatus = localStorageStore("onlineStatus", true);
 export const authenticating: Writable<boolean> = writable(false);
 
-const students: StudentLoEvent[] = [];
 export const studentsOnline = writable(0);
-export const studentsOnlineList = writable<PresenceObject>(students);
+export const studentsOnlineList = writable<LoEvent[]>([]);
