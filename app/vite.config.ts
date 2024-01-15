d898b07d756e4f8fdecd1d2e793bee1c5f73ddb9
Michael Kelly <20041540@mail.wit.ie>
2024-01-15 13:31:43 +0000

    Update vite config to match the skeleton app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 const file = fileURLToPath(new URL("package.json", import.meta.url));
 const json = readFileSync(file, "utf8");
 const pkg = JSON.parse(json);
 
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit()],
+export default defineConfig({
+  plugins: [sveltekit(), purgeCss()],
   define: {
     PKG: pkg
   }
-};
-
-export default config;
+});
