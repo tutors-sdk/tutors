a65cb785308b22b81adf65988eb216196f1ffc9e
Michael Kelly <20041540@mail.wit.ie>
2024-01-16 11:25:53 +0000

    Add tailwind purgecss to vite coinfig file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,13 +2,14 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { readFileSync } from "fs";
 import { defineConfig } from 'vite';
 import { resolve } from "path";
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 const packageJsonPath = resolve("package.json");
 const json = readFileSync(packageJsonPath, "utf8");
 
 export default defineConfig({
 
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), purgeCss()],
   define: {
     PKG: JSON.parse(json),
   },
