174d6c6075745067d256e3149a4bb1ee4a5978a2
Eamonn de Leastar <edeleastar@wit.ie>
2024-01-17 13:17:25 +0000

    remove complexity related to recovering version from package. Hard code version in footer

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-const file = fileURLToPath(new URL("package.json", import.meta.url));
-const json = readFileSync(file, "utf8");
-const pkg = JSON.parse(json);
-
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit()],
-  define: {
-    PKG: pkg
-  }
-};
-
-export default config;
-
+export default defineConfig({
+  plugins: [sveltekit()]
+});
