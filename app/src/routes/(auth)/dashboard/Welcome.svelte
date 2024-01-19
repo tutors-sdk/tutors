9abc707f4371300fffe04eda77be7ddbb027e0f0
tom <info@tcwebdesign.ie>
2024-01-19 15:00:34 +0000

    Fixed overflow of TutorsLinks buttons on mobile. Changed order of last two buttons to keep alternating colour scheme.

--- app/src/routes/(auth)/dashboard/Welcome.svelte
+++ app/src/routes/(auth)/dashboard/Welcome.svelte
@@ -10,20 +10,20 @@
       </p>
     </div>
     <div>
-      <div class="grid grid-cols-2 gap-3">
-        <div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-3 my-4">
+        <div class="mx-auto">
           <a class="btn btn-xl bg-primary-500 text-white font-bold hover:scale-105 transition-all w-60" href="/course/reference-course" target="_blank" rel="noreferrer"> Demo </a>
         </div>
-        <div>
+        <div class="mx-auto">
           <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/course/tutors-reference-manual" target="_blank" rel="noreferrer"
-            >Documentation</a
+          >Documentation</a
           >
         </div>
-        <div>
-          <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/live" target="_blank" rel="noreferrer"> Live </a>
-        </div>
-        <div>
+        <div class="mx-auto sm:order-4">
           <a class="btn btn-xl bg-primary-500 text-white font-bold hover:scale-105 transition-all w-60" href="/gallery" target="_blank" rel="noreferrer">Gallery</a>
+        </div>
+        <div class="mx-auto sm:order-3">
+          <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/live" target="_blank" rel="noreferrer"> Live </a>
         </div>
       </div>
     </div>
--- app/src/routes/(home)/TutorsLinks.svelte
+++ app/src/routes/(home)/TutorsLinks.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { modeCurrent } from "@skeletonlabs/skeleton";
+    import { modeCurrent } from "@skeletonlabs/skeleton";
 </script>
 
 <div class="bg-gradient-to-l from-primary-50 dark:from-primary-900 to-accent-50 dark:to-accent-900">
@@ -13,20 +13,20 @@
       <p class="font-bold !text-lg my-4">
         A collection of open source components & services supporting the creation of transformative learning experiences using open web standards.
       </p>
-      <div class="grid grid-cols-2 gap-3">
-        <div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-3 my-4">
+        <div class="mx-auto">
           <a class="btn btn-xl bg-primary-500 text-white font-bold hover:scale-105 transition-all w-60" href="/course/reference-course" target="_blank" rel="noreferrer"> Demo </a>
         </div>
-        <div>
+        <div class="mx-auto">
           <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/course/tutors-reference-manual" target="_blank" rel="noreferrer"
-            >Documentation</a
+          >Documentation</a
           >
         </div>
-        <div>
-          <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/live" target="_blank" rel="noreferrer"> Live </a>
-        </div>
-        <div>
+        <div class="mx-auto sm:order-4">
           <a class="btn btn-xl bg-primary-500 text-white font-bold hover:scale-105 transition-all w-60" href="/gallery" target="_blank" rel="noreferrer">Gallery</a>
+        </div>
+        <div class="mx-auto sm:order-3">
+          <a class="btn btn-xl bg-surface-100-800-token font-bold hover:scale-105 transition-all w-60" href="/live" target="_blank" rel="noreferrer"> Live </a>
         </div>
       </div>
     </div>
