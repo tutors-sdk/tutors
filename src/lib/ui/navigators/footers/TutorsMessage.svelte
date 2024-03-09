adb20f7ca3cb70f22dc6d5348b972e6d4a71552d
Eamonn de Leastar <edeleastar@wit.ie>
2024-03-09 10:49:04 +0000

    package updates

--- src/lib/ui/navigators/footers/TutorsMessage.svelte
+++ src/lib/ui/navigators/footers/TutorsMessage.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { convertMdToHtml } from "$lib/services/models/markdown-utils";
   export let content =
-    "Pitch in to the [Tutors Project](https://tutors.dev): see [*Project Readme*](https://github.com/tutors-sdk/tutors/blob/development/README.md), [*Open Issues*](https://github.com/tutors-sdk/tutors/issues) & [*Gallery*](https://tutors.dev/gallery)";
+    "Why not pitch in to the [Tutors Project](https://tutors.dev)?: see [*Project Readme*](https://github.com/tutors-sdk/tutors/blob/development/README.md), [*Open Issues*](https://github.com/tutors-sdk/tutors/issues) & [*Gallery*](https://tutors.dev/gallery)";
   let contentHtml = convertMdToHtml(content);
 </script>
 
