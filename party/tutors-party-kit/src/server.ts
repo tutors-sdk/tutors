2d0548d0769a0c29cb5d32e8e14d9c3d42b1f076
Eamonn de Leastar <edeleastar@wit.ie>
2024-05-08 08:25:03 +0100

    update partykit libraries

--- party/tutors-party-kit/src/server.ts
+++ party/tutors-party-kit/src/server.ts
@@ -1,15 +1,19 @@
 import type * as Party from "partykit/server";
 
 export default class Server implements Party.Server {
-  constructor(readonly party: Party.Party) {}
+  constructor(readonly room: Party.Room) {}
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    console.log(`Tutors connection: id: ${conn.id} room: ${this.party.id} url: ${new URL(ctx.request.url).pathname}`);
+    console.log(
+      `Tutors connection: id: ${conn.id} room: ${this.room.id} url: ${
+        new URL(ctx.request.url).pathname
+      }`
+    );
   }
 
   onMessage(message: string, sender: Party.Connection) {
     console.log(`connection ${sender.id} sent message: ${message}`);
-    this.party.broadcast(message);
+    this.room.broadcast(message);
   }
 }
 
