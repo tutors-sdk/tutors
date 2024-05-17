e750abe38dac94687b83042e85fbcdd5bffc0b94
Eamonn de Leastar <edeleastar@wit.ie>
2024-05-17 08:28:23 +0100

    reorganise the themes module

---
+++ src/lib/ui/themes/icons/hero-icons.ts
@@ -0,0 +1,59 @@
+export const HeroIconLib = {
+  // Home Icon
+  programHome: { type: "heroicons-outline:home", color: "success" },
+
+  // companion Icons
+  slack: { type: "grommet-icons:slack", color: "error" },
+  moodle: { type: "map:university", color: "warning" },
+  youtube: { type: "logos:youtube-icon", color: "error" },
+  video: { type: "heroicons-outline:play", color: "error" },
+  zoom: { type: "grommet-icons:zoom", color: "primary" },
+  teams: { type: "logos:microsoft-teams", color: "bg-base-content" },
+
+  // LoTypes icons
+  course: { type: "heroicons-outline:book-open", color: "primary" },
+  topic: { type: "heroicons-outline:view-boards", color: "primary" },
+  unit: { type: "heroicons-outline:collection", color: "success" },
+  side: { type: "heroicons-outline:collection", color: "success" },
+  talk: { type: "heroicons-outline:presentation-chart-bar", color: "primary" },
+  reference: { type: "heroicons-outline:document-duplicate", color: "warning" },
+  lab: { type: "heroicons-outline:beaker", color: "success" },
+  archive: { type: "heroicons-outline:archive", color: "primary" },
+  web: { type: "heroicons-outline:bookmark", color: "primary" },
+  github: { type: "heroicons-outline:terminal", color: "warning" },
+  panelvideo: { type: "heroicons-outline:play", color: "error" },
+  paneltalk: { type: "heroicons-outline:presentation-chart-bar", color: "primary" },
+  note: { type: "heroicons:document-text", color: "warning" },
+  panelnote: { type: "heroicons:document-text", color: "warning" },
+
+  // pdf reader icons
+  left: { type: "heroicons-outline:arrow-left", color: "success" },
+  right: { type: "heroicons-outline:arrow-right", color: "success" },
+  print: { type: "heroicons-outline:printer", color: "success" },
+  rotate: { type: "heroicons-outline:refresh", color: "success" },
+  download: { type: "heroicons-outline:arrow-down", color: "success" },
+  fullScreen: { type: "heroicons-outline:arrows-expand", color: "success" },
+
+  // tutors time icons
+  online: { type: "fluent:presence-available-24-filled", color: "success" },
+  offline: { type: "fluent:presence-available-24-regular", color: "error" },
+  tutorsTime: { type: "heroicons-outline:clock", color: "primary" },
+  timeExport: { type: "heroicons-outline:save-as", color: "success" },
+  live: { type: "heroicons-outline:users", color: "success" },
+
+  // app icons
+  search: { type: "heroicons-outline:search", color: "primary" },
+  tutors: { type: "fa-solid:chalkboard-teacher", color: "bg-base-content" },
+  logout: { type: "heroicons-outline:logout", color: "error" },
+  dark: { type: "heroicons-outline:moon", color: "warning" },
+  toc: { type: "heroicons-outline:menu-alt-2", color: "bg-base-content" },
+  compacted: { type: "heroicons-outline:dots-vertical", color: "success" },
+  expanded: { type: "heroicons-outline:dots-horizontal", color: "success" },
+  courseinfo: { type: "heroicons-outline:information-circle", color: "bg-base-content" },
+  calendar: { type: "fluent:calendar-ltr-12-regular", color: "bg-base-content" },
+  appSettings: { type: "fluent:settings-24-filled", color: "primary" },
+  listOnline: { type: "fluent:people-list-24-filled", color: "primary" },
+  edit: { type: "typcn:edit", color: "primary" },
+
+  default: { type: "heroicons-outline:dots-vertical", color: "primary" }
+};
---
+++ src/lib/ui/themes/menu/DarkModeToggle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Icon from "$lib/ui/themes/icons/Icon.svelte";
+  import { modeCurrent, setModeCurrent, setModeUserPrefers } from "@skeletonlabs/skeleton";
+  function onDarkModeToggleHandler(): void {
+    $modeCurrent = !$modeCurrent;
+    setModeUserPrefers($modeCurrent);
+    setModeCurrent($modeCurrent);
+  }
+</script>
+
+<button class="btn w-full flex justify-between" on:click={onDarkModeToggleHandler}>
+  <span class="flex-none">{$modeCurrent === true ? "Dark" : "Light"} Mode</span>
+  {#if $modeCurrent}
+    <Icon icon="fluent:weather-moon-48-filled" color="rgba(var(--color-warning-500))" />
+  {:else}
+    <Icon icon="fluent:weather-sunny-32-filled" color="rgba(var(--color-warning-500))" />
+  {/if}
+</button>
