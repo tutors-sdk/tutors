d922feaffebde2408da092c1f538d158d569c6a8
Eamonn de Leastar <edeleastar@wit.ie>
2024-05-17 08:56:50 +0100

    fix: auth screen style

--- src/routes/(auth)/auth/+page.svelte
+++ src/routes/(auth)/auth/+page.svelte
@@ -29,7 +29,7 @@
 
 <div class="bg-surface-100-800-token mx-auto mb-2 place-items-center overflow-hidden rounded-xl p-4">
   <div class="flex flex-wrap justify-center">
-    <div class="card w-4/5 !bg-surface-50 dark:!bg-surface-700 border-y-8 border-{getIcon('note').colour}-500 m-2">
+    <div class="card w-4/5 !bg-surface-50 dark:!bg-surface-700 border-y-8 border-{getIcon('note').color}-500 m-2">
       <header class="card-header flex flex-row items-center justify-between p-3">
         <div class="flex-auto text-center !text-black dark:!text-white">Tutors Sign In</div>
       </header>
@@ -37,7 +37,7 @@
         <Login {handleSignIn} />
       </footer>
     </div>
-    <div class="card w-4/5 !bg-surface-50 dark:!bg-surface-700 border-y-8 border-{getIcon('topic').colour}-500 m-2">
+    <div class="card w-4/5 !bg-surface-50 dark:!bg-surface-700 border-y-8 border-{getIcon('topic').color}-500 m-2">
       <footer class="card-footer mt-4">
         <article class="mx-auto prose dark:prose-invert max-w-none w-[80%]">
           <Terms />
