3597cbcc23b4e2cd79f9aff91d707de74ae54d76
Eamonn de Leastar <edeleastar@wit.ie>
2024-09-10 11:20:09 +0100

    remove complex unit detection

--- src/routes/(course-reader)/topic/[courseid]/[...loid]/+page.ts
+++ src/routes/(course-reader)/topic/[courseid]/[...loid]/+page.ts
@@ -7,27 +7,29 @@ export const ssr = false;
 
 export const load: PageLoad = async ({ params, url, fetch }) => {
   let topicId = url.pathname;
-  let unitId = "";
-  let unitPos = topicId.indexOf("/unit");
-  if (unitPos !== -1) {
-    unitId = topicId.slice(unitPos + 1);
-    topicId = topicId.slice(0, unitPos);
-  }
-  let sidePos = topicId.indexOf("/side");
-  if (sidePos !== -1) {
-    unitId = topicId.slice(sidePos + 1);
-    topicId = topicId.slice(0, sidePos);
-  }
-  const topic = await courseService.readTopic(params.courseid, topicId, fetch) as Topic;
-  if (unitPos !== -1) {
-    const unitLo = topic.los.filter((lo) => lo.id == unitId);
-    currentLo.set(unitLo[0]);
-  } else {
-    currentLo.set(topic);
-    unitId = "";
-  }
+  // let unitId = "";
+  // let unitPos = topicId.indexOf("/unit");
+  // if (unitPos !== -1) {
+  //   unitId = topicId.slice(unitPos + 1);
+  //   topicId = topicId.slice(0, unitPos);
+  // }
+  // let sidePos = topicId.indexOf("/side");
+  // if (sidePos !== -1) {
+  //   unitId = topicId.slice(sidePos + 1);
+  //   topicId = topicId.slice(0, sidePos);
+  // }
+  // const topic = await courseService.readTopic(params.courseid, topicId, fetch) as Topic;
+  // if (unitPos !== -1) {
+  //   const unitLo = topic.los.filter((lo) => lo.id == unitId);
+  //   currentLo.set(unitLo[0]);
+  // } else {
+  //   currentLo.set(topic);
+  //   unitId = "";
+  // }
+
+  const topic = (await courseService.readTopic(params.courseid, topicId, fetch)) as Topic;
 
   return {
-    topic: topic,
+    topic: topic
   };
 };
