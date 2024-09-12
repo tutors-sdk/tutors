94afd26346b8d77e31c6285768381b20bc6a5d5a
Eamonn de Leastar <edeleastar@wit.ie>
2024-09-12 18:08:34 +0100

    remove debug traces

--- cli/tutors-gen-lib/src/generator/resource-builder.ts
+++ cli/tutors-gen-lib/src/generator/resource-builder.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import path from "path";
-import { copyFileToFolder, findFirstMatchingString, findLastMatchingString, getFileName, getFileType } from "./file-utils";
+import { copyFileToFolder, findLastMatchingString, getFileName, getFileType } from "./file-utils";
 import { assetTypes, LearningResource, loTypes } from "../models/lo-types";
 
 const loSignatures: string[] = [];
@@ -11,11 +11,7 @@ export const resourceBuilder = {
   root: "",
 
   getLoType(route: string): string {
-    let lotype2 = findFirstMatchingString(loSignatures, route, this.root);
-    console.log(route);
-    console.log("left to right: " + lotype2);
     let lotype = findLastMatchingString(loSignatures, route, this.root);
-    console.log("right to Left: " + lotype);
     if (lotype == "book") {
       lotype = "lab";
     } else if (lotype == "") {
