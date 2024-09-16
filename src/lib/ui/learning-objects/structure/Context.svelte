c8f625c614f01ef195e9b6f91b37fb2b3680dab7
Eamonn de Leastar <edeleastar@wit.ie>
2024-09-16 07:10:06 +0100

    stick lo context

--- src/lib/ui/learning-objects/structure/Context.svelte
+++ src/lib/ui/learning-objects/structure/Context.svelte
@@ -17,8 +17,10 @@
     {/key}
   </div>
   {#if loContext}
-    <div class="hidden xl:block">
-      <LoContextPanel {loContext} />
+    <div class="hidden xl:block h-auto w-72 mr-2">
+      <div class="sticky h-auto top-6">
+        <LoContextPanel {loContext} />
+      </div>
     </div>
   {/if}
 </div>
