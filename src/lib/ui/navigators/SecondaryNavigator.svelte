fbe724a34fcad25fa5a468727076f67e9322326f
emmaroche <20088680@mail.wit.ie>
2024-10-24 16:12:05 +0100

    Implement navigation update for wall icon bar

--- src/lib/ui/navigators/SecondaryNavigator.svelte
+++ src/lib/ui/navigators/SecondaryNavigator.svelte
@@ -20,7 +20,7 @@
         <div class="my-2 hidden rounded-lg bg-primary-200 bg-opacity-80 dark:bg-primary-800 lg:flex lg:flex-none">
           <IconBar nav={$currentCourse?.companions} />
         </div>
-        <div class="my-2 ml-2 mr-10 hidden rounded-lg bg-primary-200 bg-opacity-80 dark:bg-primary-800 lg:flex lg:flex-none">
+        <div class="my-2 ml-2 mr-10 hidden sm:block rounded-lg bg-primary-200 bg-opacity-80 dark:bg-primary-800 flex flex-none">
           <IconBar nav={$currentCourse?.wallBar} />
         </div>
       {/if}
