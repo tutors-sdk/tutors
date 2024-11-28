841dd4ad868cd7de9632219366da9b4313026f8a
Eamonn de Leastar <edeleastar@wit.ie>
2024-11-28 12:06:06 +0000

    links style update

--- src/routes/(home)/Links.svelte
+++ src/routes/(home)/Links.svelte
@@ -4,34 +4,34 @@
 
 <div class="my-4 flex flex-col items-center justify-center gap-4 md:flex-row">
   <a
-    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black"
+    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black md:w-48"
     href="/course/tutors-reference-manual"
     rel="noreferrer"
   >
-    <Icon type="note" />&nbsp; Docs
+    <Icon type="note" /> Docs
   </a>
   <a
-    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black"
+    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black md:w-48"
     href="https://github.com/tutors-sdk/tutors"
     target="_blank"
     rel="noreferrer"
   >
-    <Icon type="github" />&nbsp; Source
+    <Icon type="github" /> Source
   </a>
   <a
-    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black"
+    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black md:w-48"
     href="https://live.tutors.dev/catalogue"
     target="_blank"
     rel="noreferrer"
   >
-    <Icon type="live" />&nbsp; Catalogue
+    <Icon type="live" /> Catalogue
   </a>
   <a
-    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black"
+    class="btn bg-primary-800 hover:bg-primary-600 dark:bg-primary-400 darkbg-primary-700 w-2/3 rounded text-white hover:scale-110 dark:text-black md:w-48"
     href="https://live.tutors.dev"
     target="_blank"
     rel="noreferrer"
   >
-    <Icon type="time" />&nbsp; Live
+    <Icon type="time" /> Live
   </a>
 </div>
