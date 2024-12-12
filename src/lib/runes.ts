3d4a8ae24bbab300f5725816dd91f58748dbe715
Eamonn de Leastar <edeleastar@wit.ie>
2024-12-12 08:08:05 +0000

    adjust card layout + include video icon

--- src/lib/runes.ts
+++ src/lib/runes.ts
@@ -15,8 +15,8 @@ export const currentLabStepIndex = rune(0);
 export const cardHeight = rune("380px");
 export const headingText = rune("!text-lg font-semibold");
 export const cardWidths = rune("w-60 h-[21rem]");
-export const iconHeight = rune("140");
-export const imageHeight = rune("h-32");
+export const iconHeight = rune("160");
+export const imageHeight = rune("h-40");
 export const textSize = rune("text-base");
 export const avatarWidth = rune("w-12");
 
