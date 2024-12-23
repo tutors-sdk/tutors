96f1cf7cc743ee3b5436473905ff55afdff0c916
Eamonn de Leastar <edeleastar@wit.ie>
2024-12-23 17:34:07 +0000

    remove unused runes

--- src/lib/runes.ts
+++ src/lib/runes.ts
@@ -14,14 +14,6 @@ export const currentCourse = rune<Course | null>(null);
 export const courseUrl = rune("");
 export const currentLabStepIndex = rune(0);
 
-export const cardHeight = rune("380px");
-export const headingText = rune("!text-lg font-semibold");
-export const cardWidths = rune("w-60 h-[21rem]");
-export const iconHeight = rune("160");
-export const imageHeight = rune("h-40");
-export const textSize = rune("text-base");
-export const avatarWidth = rune("w-12");
-
 export const adobeLoaded = rune(false);
 
 export const animationDelay = rune(200);
