f824f7f3e78a0144bf093c978047fbbe5e3602ad
Eamonn de Leastar <edeleastar@wit.ie>
2024-12-28 10:01:49 +0000

    remove currentWallType

--- src/lib/runes.ts
+++ src/lib/runes.ts
@@ -1,8 +1,6 @@
 import type { Lo, Course } from "$lib/services/models/lo-types";
 import { rune } from "./services/utils/runes.svelte";
 
-export const currentWallType = rune("");
-
 export const currentLo = rune<Lo | null>(null);
 export const currentCourse = rune<Course | null>(null);
 export const courseUrl = rune("");
