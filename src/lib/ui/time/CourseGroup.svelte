f0fdc6f0e6769791249270c7ff10da9011e793ff
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-05 14:20:36 +0000

    time components

---
+++ src/lib/ui/time/CourseGroup.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { LoRecord } from "$lib/services/types.svelte";
+  import Card from "../components/Card.svelte";
+  export let los: LoRecord[];
+</script>
+
+<div
+  class="bg-surface-100-800-token border-surface-200-700-token mx-auto mb-2 w-full place-items-center overflow-hidden rounded-xl border-[1px] p-4"
+>
+  <div class="flex w-full justify-between pb-2">
+    <h2 class="p-2">
+      {los[0].courseTitle}
+    </h2>
+  </div>
+  <div class="flex flex-wrap justify-center">
+    {#each los as lo}
+      <Card
+        cardDetails={{
+          route: lo?.loRoute,
+          student: lo?.user,
+          title: lo?.courseTitle,
+          type: lo?.type,
+          summary: lo?.title,
+          img: lo?.img,
+          icon: lo?.icon
+        }}
+      />
+    {/each}
+  </div>
+</div>
---
+++ src/lib/ui/time/Students.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { liveService } from "$lib/services/live.svelte";
+  import Card from "../components/Card.svelte";
+</script>
+
+<div class="flex flex-wrap justify-center">
+  {#each liveService.studentsOnline.value as lo}
+    <Card
+      cardDetails={{
+        route: lo?.loRoute,
+        student: lo?.user,
+        type: lo?.type,
+        summary: lo?.courseTitle,
+        title: lo?.courseTitle,
+        summaryEx: lo?.title + " (" + lo?.type + ")",
+        img: lo?.img,
+        icon: lo?.icon
+      }}
+    />
+  {/each}
+</div>
