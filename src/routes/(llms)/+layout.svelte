85296ce8e8efd63ae260e329e2667d40186a8af2
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-11 14:54:14 +0000

    placeholder

---
+++ src/routes/(llms)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { currentCourse } from "$lib/runes.svelte";
+  import CourseShell from "$lib/ui/TutorsShell.svelte";
+  import type { Snippet } from "svelte";
+  type Props = { children: Snippet };
+  let { children }: Props = $props();
+
+  currentCourse.value = null;
+</script>
+
+<svelte:head>
+  <title>Tutors</title>
+</svelte:head>
+
+<CourseShell>
+  {@render children()}
+</CourseShell>
---
+++ src/routes/(llms)/llms/[courseid]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { convertMdToHtml } from "$lib/services/markdown";
+  import Composite from "$lib/ui/learning-objects/structure/Composite.svelte";
+
+  interface Props {
+    data: any;
+  }
+  let { data }: Props = $props();
+  let text = `
+# Docs for LLMs
+
+We support the [llms.txt](https://llmstxt.org/) convention for making documentation available to large language models and the applications that make use of them.
+
+Currently, we have the following root-level files...
+
+- /llms.txt — a listing of the available files
+- /llms-full.txt — complete documentation for Svelte, SvelteKit and the CLI
+`;
+  let textmd = convertMdToHtml(text);
+</script>
+
+<div class="container mx-auto mt-2 items-center justify-between lg:flex">
+  <div class="mx-4">
+    <div class="prose dark:prose-invert">
+      {@html textmd}
+    </div>
+  </div>
+</div>
