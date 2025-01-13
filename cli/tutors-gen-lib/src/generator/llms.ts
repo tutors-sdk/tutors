2e7ba0761e696355d046261160cf03dc2987d2b7
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-13 10:09:13 +0000

    first complete version

--- cli/tutors-gen-lib/src/generator/llms.ts
+++ cli/tutors-gen-lib/src/generator/llms.ts
@@ -1,25 +1,65 @@
 import { Lo, Lab, Course, isCompositeLo, Composite } from "../models/lo-types";
+import { flattenLos } from "../models/lo-utils";
 import { writeFile } from "./file-utils";
 
+let header = "";
 
-function generateLoLlms (lo: Lo, llmsFullTxt: string[]) {
-  llmsFullTxt.push (`${lo.type}: ${lo.title}\n\n${lo.contentMd}`);
-  if (isCompositeLo(lo)) {
+function generateLo(lo: Lo, llmsTxt: string[], level: number) {
+  llmsTxt.push(`${lo.title}\n\n${lo.contentMd}`);
+  if (lo.hasOwnProperty("los")) {
     const compositeLo = lo as Composite;
-    compositeLo.los?.forEach((childLo:Lo) => {
-      generateLoLlms(childLo, llmsFullTxt);
+    compositeLo.los?.forEach((childLo: Lo) => {
+      generateLo(childLo, llmsTxt, level + 1);
     });
   }
 }
 
+function generateAll(course: Course, folder: string) {
+  const allTxt = [];
+  allTxt.push(header);
+  allTxt.push(`# ${course.title}\n\n${course.contentMd}`);
+  generateLo(course, allTxt, 1);
+  return allTxt;
+}
+
+function generateNotes(notes: Lo[]): string[] {
+  const notesTxt = [];
+  notesTxt.push(header);
+  notesTxt.push("# All Notes in the course");
+  notes.forEach((note) => {
+    notesTxt.push(`${note.title}\n\n${note.contentMd}`);
+  });
+  return notesTxt;
+}
+
+function generateLabs(labs: Lo[]): string[] {
+  const labsTxt = [];
+  labsTxt.push(header);
+  labsTxt.push("# All Labs in the course");
+  labs.forEach((lab) => {
+    labsTxt.push(`${lab.title}\n\n${lab.contentMd}`);
+    if (lab.hasOwnProperty("los")) {
+      const compositeLo = lab as Composite;
+      compositeLo.los?.forEach((step) => {
+        labsTxt.push(`${step.title}\n\n${step.contentMd}`);
+      });
+    }
+  });
+  return labsTxt;
+}
+
 export function generateLlms(course: Course, folder: string) {
+  header = `<SYSTEM> This is the Tutors course ${course.title} by ${course.properties.credits}</SYSTEM>\n\n`;
 
-  const llmsFullTxt : string[] = [];
-  llmsFullTxt.push (`<SYSTEM> This is the Tutors course ${course.title} by ${course.properties.credits}</SYSTEM>`);
+  const allLos = flattenLos(course.los);
+  const labs = allLos.filter((lo) => lo.type === "lab");
+  const notes = allLos.filter((lo) => lo.type === "note");
 
-  llmsFullTxt.push (`# ${course.title}\n\n${course.contentMd}`);
- 
-  generateLoLlms(course, llmsFullTxt);
+  const allTxt = generateAll(course, folder);
+  const notesTxt = generateNotes(notes);
+  const labsTxt = generateLabs(labs);
 
-  writeFile(folder, "llms-full.txt", llmsFullTxt.join("\n"));
+  writeFile(folder, "llms-full.txt", allTxt.join("\n"));
+  writeFile(folder, "llms-labs.txt", labsTxt.join("\n"));
+  writeFile(folder, "llms-notes.txt", notesTxt.join("\n"));
 }
