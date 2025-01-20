02a99dd0e0a09104bc8b08199c55acc9e918c5b8
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-20 07:05:58 +0000

    generate archive of pdfs

--- cli/tutors-gen-lib/src/generator/llms.ts
+++ cli/tutors-gen-lib/src/generator/llms.ts
@@ -1,13 +1,13 @@
-import { Lo, Course, Composite } from "../models/lo-types";
+import { Lo, Course, Composite, Talk } from "../models/lo-types";
 import { flattenLos, removeFirstLine, removeLeadingHashes } from "../models/lo-utils";
-import { writeFile } from "./file-utils";
+import { compressToZip, writeFile } from "./file-utils";
 
 let header = "";
 
-function printLo (lo: Lo, level: number) {
+function printLo(lo: Lo, level: number) {
   const title = removeLeadingHashes(lo.title);
   const contentMd = removeFirstLine(lo.contentMd);
-  return `${'#'.repeat(level)} ${lo.type.charAt(0).toUpperCase() + lo.type.slice(1)} : ${title}\n\n ${contentMd}`;
+  return `${"#".repeat(level)} ${lo.type.charAt(0).toUpperCase() + lo.type.slice(1)} : ${title}\n\n ${contentMd}`;
 }
 
 function generateLo(lo: Lo, llmsTxt: string[], level: number) {
@@ -54,18 +54,36 @@ function generateLabs(labs: Lo[]): string[] {
   return labsTxt;
 }
 
+function generatePdfs(pdfs: Talk[], folder: string): string[] {
+  const pdfsTxt: string[] = [];
+  const pdfFiles: string[] = [];
+  for (const pdf of pdfs) {
+    let descriptor = `# ${pdf.title} \n ${pdf.summary} \n`;
+    const relativeRoute = pdf.route.substring(pdf.route.indexOf("{{COURSEURL}}") + "{{COURSEURL}}".length);
+    descriptor += `- [${pdf.title}](https://full-stack-1-2023.netlify.app${relativeRoute}/${pdf.pdfFile})\n\n`;
+    pdfFiles.push(`./${relativeRoute}/${pdf.pdfFile}`);
+    pdfsTxt.push(descriptor);
+  }
+  compressToZip(pdfFiles, `${folder}/llms-pdfs.zip`);
+  return pdfsTxt;
+}
+
 export function generateLlms(course: Course, folder: string) {
   header = `<SYSTEM> This is the Tutors course ${course.title} by ${course.properties.credits}</SYSTEM>\n\n`;
 
   const allLos = flattenLos(course.los);
   const labs = allLos.filter((lo) => lo.type === "lab");
   const notes = allLos.filter((lo) => lo.type === "note");
+  const pdfs = allLos.filter((lo) => lo.type === "talk");
+  const videos = allLos.filter((lo) => lo.type === "video" || lo.type === "panelvideo");
 
   const allTxt = generateAll(course, folder);
   const notesTxt = generateNotes(notes);
   const labsTxt = generateLabs(labs);
+  const pdfsTxt = generatePdfs(pdfs as Talk[], folder);
 
   writeFile(folder, "llms-full.txt", allTxt.join("\n"));
   writeFile(folder, "llms-labs.txt", labsTxt.join("\n"));
   writeFile(folder, "llms-notes.txt", notesTxt.join("\n"));
+  writeFile(folder, "llms-pdfs.txt", pdfsTxt.join("\n"));
 }
