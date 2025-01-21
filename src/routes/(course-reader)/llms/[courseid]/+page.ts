b224cc6329d4a2f78e9ba5893a4dd1acbce340f0
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-21 18:42:41 +0000

    llms page update

--- src/routes/(course-reader)/llms/[courseid]/+page.ts
+++ src/routes/(course-reader)/llms/[courseid]/+page.ts
@@ -1,14 +1,17 @@
 import { currentCourse } from "$lib/runes.svelte";
 import { courseService } from "$lib/services/course";
+import { flattenLos } from "$lib/services/course/utils/lo-utils";
 
 export const ssr = false;
 
 export const load = async ({ params, fetch }) => {
   const course = await courseService.readCourse(params.courseid, fetch);
   currentCourse.value = course;
-
+  const allLos = flattenLos(course.los);
+  const videos = allLos.filter((lo) => lo.type === "panelvideo");
   return {
     course,
-    lo: course
+    lo: course,
+    videos
   };
 };
