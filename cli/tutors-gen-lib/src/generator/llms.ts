391158d4f4d8656bb62339d20c1383bcdf4615da
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-22 09:38:56 +0000

    simplify generation

--- cli/tutors-gen-lib/src/generator/llms.ts
+++ cli/tutors-gen-lib/src/generator/llms.ts
@@ -4,6 +4,10 @@ import { compressToZip, writeFile } from "./file-utils";
 
 let header = "";
 
+export function toSnakeCase(str: string): string {
+  return str.toLowerCase().replace(/[^a-z0-9\s]/g, '').replace(/\s+/g, '-');
+}
+
 function printLo(lo: Lo, level: number) {
   const title = removeLeadingHashes(lo.title);
   const contentMd = removeFirstLine(lo.contentMd);
@@ -20,7 +24,7 @@ function generateLo(lo: Lo, llmsTxt: string[], level: number) {
   }
 }
 
-function generateAll(course: Course, folder: string) {
+function generateAll(course: Course) {
   const allTxt = [];
   allTxt.push(header);
   allTxt.push(`# ${course.title}\n\n${course.contentMd}`);
@@ -28,45 +32,13 @@ function generateAll(course: Course, folder: string) {
   return allTxt;
 }
 
-function generateNotes(notes: Lo[]): string[] {
-  const notesTxt = [];
-  notesTxt.push(header);
-  notesTxt.push("# All Notes in the course");
-  notes.forEach((note) => {
-    notesTxt.push(printLo(note, 2));
-  });
-  return notesTxt;
-}
-
-function generateLabs(labs: Lo[]): string[] {
-  const labsTxt = [];
-  labsTxt.push(header);
-  labsTxt.push("# All Labs in the course");
-  labs.forEach((lab, index) => {
-    labsTxt.push(printLo(lab, 2));
-    if (lab.hasOwnProperty("los")) {
-      const compositeLo = lab as Composite;
-      compositeLo.los?.forEach((step) => {
-        labsTxt.push(printLo(step, 3));
-      });
-    }
-  });
-  return labsTxt;
-}
-
-function generatePdfs(pdfs: Talk[], folder: string): string[] {
-  const pdfsTxt: string[] = [];
+function generatePdfs(pdfs: Talk[], folder: string) {
   let pdfFiles: string[] = [];
   for (const pdf of pdfs) {
-    let descriptor = `# ${pdf.title} \n ${pdf.summary} \n`;
     const relativeRoute = pdf.route.substring(pdf.route.indexOf("{{COURSEURL}}") + "{{COURSEURL}}".length);
-    descriptor += `- [${pdf.title}](https://full-stack-1-2023.netlify.app${relativeRoute}/${pdf.pdfFile})\n\n`;
     pdfFiles.push(`./${relativeRoute}/${pdf.pdfFile}`);
-    pdfsTxt.push(descriptor);
   }
-
   compressToZip(pdfFiles, folder);
-  return pdfsTxt;
 }
 
 export function generateLlmsByTopic(course: Course, folder: string) {
@@ -77,12 +49,16 @@ export function generateLlmsByTopic(course: Course, folder: string) {
       header = `<SYSTEM> This is the Tutors course ${course.title} topic ${topic.title} by ${course.properties.credits}</SYSTEM>\n\n`;
       allTxt.push(header);
       generateLo(topic, allTxt, 1);
+
       const paddedIndex = index.toString().padStart(2, "0");
-      writeFile(folder, `${paddedIndex}-llms-full.txt`, allTxt.join("\n"));
+      const title = `${paddedIndex}-${toSnakeCase(topic.title)}`;
+
+
+      writeFile(folder, `${title}-llms.txt`, allTxt.join("\n"));
 
       const allLos = flattenLos(topic.los);
       const talks = allLos.filter((lo) => lo.type === "talk");
-      generatePdfs(talks as Talk[], `${folder}/${paddedIndex}-pdfs.zip`);
+      generatePdfs(talks as Talk[], `${folder}/${title}-pdfs.zip`);
     }
   });
 }
@@ -92,19 +68,12 @@ export function generateLlms(course: Course, folder: string) {
   header = `<SYSTEM> This is the Tutors course ${course.title} by ${course.properties.credits}</SYSTEM>\n\n`;
 
   const allLos = flattenLos(course.los);
-  const labs = allLos.filter((lo) => lo.type === "lab");
-  const notes = allLos.filter((lo) => lo.type === "note");
   const pdfs = allLos.filter((lo) => lo.type === "talk");
+  const fileName = toSnakeCase(course.title);
 
-  const allTxt = generateAll(course, llmFolder);
-  const notesTxt = generateNotes(notes);
-  const labsTxt = generateLabs(labs);
-  const pdfsTxt = generatePdfs(pdfs as Talk[], `${llmFolder}/pdfs.zip`);
-
-  writeFile(llmFolder, "llms-full.txt", allTxt.join("\n"));
-  writeFile(llmFolder, "llms-labs.txt", labsTxt.join("\n"));
-  writeFile(llmFolder, "llms-notes.txt", notesTxt.join("\n"));
-  writeFile(llmFolder, "llms-pdfs.txt", pdfsTxt.join("\n"));
+  const completeTxt = generateAll(course);
+  generatePdfs(pdfs as Talk[], `${llmFolder}/${fileName}-complete-pdfs.zip`);
+  writeFile(llmFolder, `${fileName}-complete-llms.txt`, completeTxt.join("\n"));
 
   generateLlmsByTopic(course, `${llmFolder}/topics`);
 }
