eae459c394519e46c2f52d28f429343cb9ecff0b
Eamonn de Leastar <edeleastar@wit.ie>
2025-01-24 13:19:17 +0000

    corrections to llm page

--- src/routes/(course-reader)/llm/[courseid]/+page.svelte
+++ src/routes/(course-reader)/llm/[courseid]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import { currentCourse } from "$lib/runes.svelte";
+  import { convertMdToHtml } from "$lib/services/markdown";
 
   interface Props {
     data: any;
@@ -10,12 +10,22 @@
   if (data.course.llm === 0) {
     goto(`/course/${data.course.courseId}`);
   }
+
+  const headding = `
+## Docs for LLMs
+
+Tutors supports the [llms.txt](https://llmstxt.org/) convention for making documentation available to large language models and the applications that make use of them.
+
+### Complete Course Content
+`;
+  const headdingHtml = convertMdToHtml(headding);
 </script>
 
 <div class="container mx-auto mt-2 items-center justify-between lg:flex">
   <div class="mx-4">
     <div class="prose dark:prose-invert">
-      {@html data.page}
+      {@html headdingHtml}
+      {@html data.llmsLinks}
     </div>
   </div>
 </div>
