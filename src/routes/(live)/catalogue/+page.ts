119771f68bb13f95ba0efe4bb17f05e1334075b4
Eamonn de Leastar <edeleastar@wit.ie>
2025-02-02 17:55:32 +0000

    restructure catalogue

--- src/routes/(live)/catalogue/+page.ts
+++ src/routes/(live)/catalogue/+page.ts
@@ -3,9 +3,12 @@ import { catalogueService } from "$lib/services/community";
 export const load = async ({ fetch }) => {
   let catalogueEntries = await catalogueService.getCatalogue();
   catalogueEntries = catalogueEntries
-    .filter((entry) => entry.visit_count >= 4)
+    .filter((entry) => entry.visit_count >= 50)
     .sort((a, b) => b.visit_count - a.visit_count);
-  const courseRecords = catalogueEntries.map((entry) => entry.course_record);
+  const courseRecords = catalogueEntries.map((entry) => ({
+    ...entry.course_record,
+    visits: entry.visit_count
+  }));
   return {
     courseRecords
   };
