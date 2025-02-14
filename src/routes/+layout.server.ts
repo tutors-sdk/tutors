90e9df05b7e9594c01864a370b60d6acdebfa8f3
Eamonn de Leastar <edeleastar@wit.ie>
2025-02-14 18:28:15 +0000

    switch to auth from session api

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 export const load = async ({ locals }) => {
-  const session = await locals?.getSession();
+  const session = await locals?.auth();
   const loggedIn = !!session?.user;
   const user = session?.user;
   return {
