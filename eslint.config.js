73f6ac745965569ee20d925e0ec861108bf34830
Eamonn de Leastar <edeleastar@wit.ie>
2025-02-28 06:45:21 +0000

    tailwind 4 major config overhaul

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,34 @@
-import prettier from "eslint-config-prettier";
-import js from "@eslint/js";
-import svelte from "eslint-plugin-svelte";
-import globals from "globals";
-import ts from "typescript-eslint";
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs["flat/recommended"],
-  prettier,
-  ...svelte.configs["flat/prettier"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  },
-  {
-    files: ["**/*.svelte"],
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
 
-    languageOptions: {
-      parserOptions: {
-        parser: ts.parser
-      }
-    }
-  },
-  {
-    ignores: ["build/", ".svelte-kit/", "dist/"]
-  }
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	}
 );
