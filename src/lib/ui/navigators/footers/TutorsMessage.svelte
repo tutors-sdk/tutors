d8631cfd0d3f90fdd6680ff8ecf01f97b2d108a0
Eamonn de Leastar <edeleastar@wit.ie>
2025-04-19 08:13:40 +0100

    enable easter theme

--- src/lib/ui/navigators/footers/TutorsMessage.svelte
+++ src/lib/ui/navigators/footers/TutorsMessage.svelte
@@ -6,14 +6,15 @@
   onMount(async () => {
     contentHtml = convertMdToHtml(
       // "An [Open Learning Web Toolkit](/course/tutors-reference-manual): Explore the [Tutors Catalogue](/catalogue)"
-      "An [Open Learning Web Toolkit](/course/tutors-reference-manual): Light and dark mode + themes available on the Layout menu"
+      //"An [Open Learning Web Toolkit](/course/tutors-reference-manual): Light and dark mode + themes available on the Layout menu 🥚"
+      "🐰🐰🐰  🥚🥚🥚 🐰🐰🐰 🥚🥚🥚 🐰🐰🐰  🥚🥚🥚"
     );
   });
 </script>
 
 {#if contentHtml}
   <div class="flex w-full justify-center">
-    <p class="prose prose-sm prose-slate min-w-full dark:prose-invert">
+    <p class="prose prose-sm prose-slate dark:prose-invert min-w-full">
       {@html contentHtml}
     </p>
   </div>
