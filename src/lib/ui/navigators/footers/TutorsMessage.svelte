f6769b78e806da1c5535fbeae1f2391cad3f9a59
Eamonn de Leastar <edeleastar@wit.ie>
2025-04-26 16:37:48 +0100

    footer update

--- src/lib/ui/navigators/footers/TutorsMessage.svelte
+++ src/lib/ui/navigators/footers/TutorsMessage.svelte
@@ -5,9 +5,9 @@
 
   onMount(async () => {
     contentHtml = convertMdToHtml(
-      // "An [Open Learning Web Toolkit](/course/tutors-reference-manual): Explore the [Tutors Catalogue](/catalogue)"
+      "An [Open Learning Web Toolkit](/course/tutors-reference-manual): Explore the [Tutors Catalogue](/catalogue)"
       //"An [Open Learning Web Toolkit](/course/tutors-reference-manual): Light and dark mode + themes available on the Layout menu 🥚"
-      "🐰🐰🐰  🥚🥚🥚 🐰🐰🐰 🥚🥚🥚 🐰🐰🐰  🥚🥚🥚"
+      //"🐰🐰🐰  🥚🥚🥚 🐰🐰🐰 🥚🥚🥚 🐰🐰🐰  🥚🥚🥚"
     );
   });
 </script>
