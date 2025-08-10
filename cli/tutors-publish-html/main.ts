401f7aa1938b90fe96dd51ed3e76cf26eafb6986
Eamonn de Leastar <edeleastar@wit.ie>
2025-08-10 09:21:31 +0100

    revert to repo hosted templates

--- cli/tutors-publish-html/main.ts
+++ cli/tutors-publish-html/main.ts
@@ -7,6 +7,7 @@ import * as fs from "node:fs";
 import process from "node:process";
 
 const versionStr = `tutors-publish-html: 4.1.3`;
+const srcVentoFolder = "/Users/edeleastar/repos/tutor-sdk/apps/tutors-apps/cli/tutors-gen-lib/src/templates/vento"; 
 
 if (!fs.existsSync("course.md")) {
   console.log("Cannot locate course.md. Please change to course folder and try again.");
@@ -14,7 +15,7 @@ if (!fs.existsSync("course.md")) {
   const srcFolder = process.cwd();
   const destFolder = `${srcFolder}/html`;
   const [course, lr] = parseCourse(srcFolder);
-  generateStaticCourse(course, destFolder);
+  generateStaticCourse(course, destFolder);//, srcVentoFolder);
   copyAssets(lr, destFolder);
 }
 console.log(versionStr);
