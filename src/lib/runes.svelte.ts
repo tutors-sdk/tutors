bda6222cc91743375a8c43c89225baa384880587
Eamonn de Leastar <edeleastar@wit.ie>
2025-11-12 17:53:22 +0100

    correct import

--- src/lib/runes.svelte.ts
+++ src/lib/runes.svelte.ts
@@ -1,5 +1,6 @@
 import type { TutorsId } from "$lib/services/connect";
-import type { Course, Lo } from "./services/base/lo-types";
+import type { Course, Lo } from "@tutors/tutors-model-lib";
+
 
 export const rune = <T>(initialValue: T) => {
   let _rune = $state(initialValue);
