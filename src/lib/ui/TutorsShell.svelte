568c737e20e14967e8f9cde560df180bdd424d6b
Eamonn de Leastar <edeleastar@wit.ie>
2026-04-27 10:02:26 +0100

    remove main navigator when labs displayed

--- src/lib/ui/TutorsShell.svelte
+++ src/lib/ui/TutorsShell.svelte
@@ -2,7 +2,9 @@
   import Footer from "$lib/ui/navigators/footers/Footer.svelte";
   import { onMount, type Snippet } from "svelte";
   import MainNavigator from "./navigators/MainNavigator.svelte";
-  import { slide } from "svelte/transition";
+  import { animationDelay, hideMainNavigator } from "$lib/runes.svelte";
+  import { cubicIn, cubicOut } from "svelte/easing";
+  import { fly, slide } from "svelte/transition";
 
   type Props = { children: Snippet };
   let { children }: Props = $props();
@@ -15,7 +17,15 @@
 
 <div class="flex h-screen flex-col">
   <header class="bg-surface-100 dark:bg-surface-950 sticky top-0 z-10">
-    <MainNavigator />
+    {#if !hideMainNavigator.value}
+      <div
+        class="w-full"
+        in:fly={{ y: -48, duration: animationDelay.value * 2, easing: cubicOut }}
+        out:fly={{ y: -48, duration: animationDelay.value * 2, easing: cubicIn }}
+      >
+        <MainNavigator />
+      </div>
+    {/if}
   </header>
   <main class="flex-1 overflow-y-auto">
     {@render children()}
