176b91a346733f451cb8da9b78ee2522710f0dda
Eamonn de Leastar <edeleastar@wit.ie>
2026-07-19 12:35:19 +0100

    renable support for iframes for video and podcast

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -1,5 +1,8 @@
 import DOMPurify from "dompurify";
 
 export function sanitizeHtml(html: string): string {
-  return DOMPurify.sanitize(html);
+  return DOMPurify.sanitize(html, {
+    ADD_TAGS: ["iframe", "video", "source", "audio", "embed"],
+    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "loading", "controls", "poster", "autoplay", "src", "type", "width", "height", "style", "title", "data-testid"]
+  });
 }
