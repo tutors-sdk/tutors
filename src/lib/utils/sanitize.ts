4e8bd331683d7ee5b3df5f8bff63f5e0eaf93907
Eamonn de Leastar <edeleastar@wit.ie>
2026-07-19 12:51:40 +0100

    support opening links in external tab

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -3,6 +3,6 @@ import DOMPurify from "dompurify";
 export function sanitizeHtml(html: string): string {
   return DOMPurify.sanitize(html, {
     ADD_TAGS: ["iframe", "video", "source", "audio", "embed"],
-    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "loading", "controls", "poster", "autoplay", "src", "type", "width", "height", "style", "title", "data-testid"]
+    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "loading", "controls", "poster", "autoplay", "src", "type", "width", "height", "style", "title", "data-testid", "target"]
   });
 }
